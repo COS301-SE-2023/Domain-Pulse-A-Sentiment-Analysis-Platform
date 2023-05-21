1163f0f617f64afb922f81778b9b8ce32d373057
Luke Lawson <u21433811@tuks.co.za>
2023-05-21 12:37:14 +0200

    ✨ (backend) added logic for domain management

--- backend/engine/domains/views.py
+++ backend/engine/domains/views.py
@@ -1,19 +1,20 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from utils import domainscrud
 
 # Create your views here.
 
 def add_domain(request, user_id, domain_name):
-    return JsonResponse({"worked":"yes"})
+    return JsonResponse(domainscrud.add_domain(user_id, domain_name))
 
 def remove_domain(request, user_id, domain_id):
-    return JsonResponse({"worked":"yes"})
+    return JsonResponse(domainscrud.remove_domain(user_id, domain_id))
 
 def add_source(request, user_id, domain_id, source_name):
-    return JsonResponse({"worked":"yes"})
+    return JsonResponse(domainscrud.add_source(user_id, domain_id, source_name))
 
 def remove_source(request, user_id, domain_id, source_id):
-    return JsonResponse({"worked":"yes"})
+    return JsonResponse(domainscrud.remove_source(user_id, domain_id, source_id))
 
 def get_domains(request, user_id):
-    return JsonResponse({"worked":"yes"})
+    return JsonResponse(domainscrud.get_domains(user_id))
