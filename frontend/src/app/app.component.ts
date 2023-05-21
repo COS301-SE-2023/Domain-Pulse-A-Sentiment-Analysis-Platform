f49d8582d6576434b90b5847980ba984171f6b02
Thuthuka Khumalo <thuthuka111@gmail.com>
2023-05-21 14:00:41 +0200

    💄 (app) add sources modal

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Domain Pulse';
+
+  showAddSourcesModal = false;
+
+  toggleAddSourcesModal() {
+    if (!this.showAddSourcesModal) {
+      // this.windows[0].scrolling = false;
+      this.showAddSourcesModal = true;
+    } else {
+      // this.windows[0].scrolling = true;
+      this.showAddSourcesModal = false;
+    }
+  }
 }
