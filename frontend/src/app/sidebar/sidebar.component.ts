d4ef977196ac4e87f79be552361d8d7961a91510
Thuthuka Khumalo <thuthuka111@gmail.com>
2023-05-21 12:05:24 +0200

    💄 (app) style for domains

--- frontend/src/app/sidebar/sidebar.component.ts
+++ frontend/src/app/sidebar/sidebar.component.ts
@@ -3,8 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'dp-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.sass']
+  styleUrls: ['./sidebar.component.sass'],
 })
 export class SidebarComponent {
-
+  domains = [
+    {
+      name: 'apple',
+      imageUrl: '../assets/apple-1-logo-png-transparent.png',
+      selected: false,
+    },
+    {
+      name: 'Starbucks',
+      imageUrl: '../assets/starbucks-logo-69391AB0A9-seeklogo.com.png',
+      selected: true,
+    },
+    {
+      name: 'Mcdonalds',
+      imageUrl: '../assets/donalds-logo.png',
+      selected: false,
+    },
+    {
+      name: 'Formula 1',
+      imageUrl: '../assets/f1-logo.png',
+      selected: false,
+    },
+  ];
 }
