5b2e3008d9db71db2d181d58d7f74b22abff024c
Thuthuka Khumalo <thuthuka111@gmail.com>
2023-05-22 22:30:38 +0200

    🚑(app) import correct modules in test

--- frontend/src/app/app.component.spec.ts
+++ frontend/src/app/app.component.spec.ts
@@ -1,16 +1,16 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { BackendService } from './backend.service';
+import { HttpClientModule } from '@angular/common/http';
+import { ModalContainerComponent } from './modal-container/modal-container.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [HttpClientModule, RouterTestingModule],
+      declarations: [AppComponent, ModalContainerComponent, SidebarComponent],
     }).compileComponents();
   });
 
--- frontend/src/app/backend.service.spec.ts
+++ frontend/src/app/backend.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { BackendService } from './backend.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('BackendService', () => {
   let service: BackendService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule],
+    });
     service = TestBed.inject(BackendService);
   });
 
