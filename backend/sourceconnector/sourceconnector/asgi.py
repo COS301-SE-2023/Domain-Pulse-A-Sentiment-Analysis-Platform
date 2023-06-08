423e07c638928d5d4d51d95d7689367d2a106fae
Luke Lawson <u21433811@tuks.co.za>
2023-06-08 08:58:24 +0200

    🚧 (backend) Initial architecture refactor

--- backend/sourceconnector/sourceconnector/asgi.py
+++ backend/sourceconnector/sourceconnector/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for profilemanager project.
+ASGI config for sourceconnector project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'profilemanager.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sourceconnector.settings')
 
 application = get_asgi_application()
--- backend/sourceconnector/sourceconnector/wsgi.py
+++ backend/sourceconnector/sourceconnector/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for profilemanager project.
+WSGI config for sourceconnector project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'profilemanager.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sourceconnector.settings')
 
 application = get_wsgi_application()
