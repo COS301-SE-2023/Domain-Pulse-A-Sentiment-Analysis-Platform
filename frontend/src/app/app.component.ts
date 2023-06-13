c70232f6e4ecad602764688bb4cfdf024c45b202
Thuthuka Khumalo <thuthuka111@gmail.com>
2023-06-13 20:44:53 +0200

    ✨ (frontend) expandable sidebar

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
 import { BackendService } from './backend.service';
-
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition,
+} from '@angular/animations';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass'],
+  animations: [
+    trigger('collapsed', [
+      state(
+        'collapsed',
+        style({
+          maxWidth: '5%',
+        })
+      ),
+      state(
+        'notCollapsed',
+        style({
+          maxWidth: '20%',
+        })
+      ),
+      transition('collapsed <=> notCollapsed', [animate('0.3s ease-in-out')]),
+    ]),
+  ],
 })
 export class AppComponent {
+  sidebarCollapsed = true;
   title = 'Domain Pulse';
 
   showAddSourcesModal = false;
@@ -33,5 +57,15 @@ export class AppComponent {
       // this.windows[0].scrolling = true;
       this.showAddSourcesModal = false;
     }
+  }
+
+  openSidebar() {
+    this.sidebarCollapsed = false;
+  }
+
+  colapseSidebar() {
+    setTimeout(() => {
+      this.sidebarCollapsed = true;
+    }, 300);
   }
 }
