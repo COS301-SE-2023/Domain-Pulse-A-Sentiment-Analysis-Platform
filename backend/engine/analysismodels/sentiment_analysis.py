66bbfdde76027f3d934cd2bdd073f9e0a70b02fc
Luke Lawson <u21433811@tuks.co.za>
2023-06-22 15:54:27 +0200

    ✨ (backend) Added aggregator service

--- backend/engine/analysismodels/sentiment_analysis.py
+++ backend/engine/analysismodels/sentiment_analysis.py
@@ -1,5 +1,6 @@
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 from preprocessor import preprocessing
+from postprocessor import aggregation
 from utils import mock_data
 from transformers import pipeline
 
@@ -139,94 +140,6 @@ def analyse_content(data):
     return metrics
 
 
-def aggregate_sentiment_data(sentiment_data):
-    sentiment_data = list(sentiment_data)
-
-    num_individuals = len(sentiment_data)
-
-    summed_overall_score = 0
-
-    emotions = {
-        "anger": 0,
-        "disgust": 0,
-        "fear": 0,
-        "joy": 0,
-        "neutral": 0,
-        "sadness": 0,
-        "surprise": 0,
-    }
-
-    summed_positive_ratio = 0
-    summed_neutral_ratio = 0
-    summed_negative_ratio = 0
-
-    summed_toxicity = 0
-
-    for item in sentiment_data:
-        # General
-        summed_overall_score += item["general"]["score"]
-
-        # Emotions
-        for label, score in dict(item["emotions"]).items():
-            emotions[label] += score
-
-        # Toxicity
-        summed_toxicity += item["toxicity"]["score"]
-
-        # Ratio
-        summed_positive_ratio += item["ratios"]["positive"]
-        summed_neutral_ratio += item["ratios"]["neutral"]
-        summed_negative_ratio += item["ratios"]["negative"]
-
-    agg_overall_score = round(summed_overall_score / num_individuals, 4)
-    overall_cat = ""
-    if 0.5 <= agg_overall_score < 0.625:
-        overall_cat = "SOMEWHAT_POSITIVE"
-    elif 0.625 <= agg_overall_score < 0.875:
-        overall_cat = "POSITIVE"
-    elif 0.875 <= agg_overall_score:
-        overall_cat = "VERY_POSITIVE"
-    elif 0.5 > agg_overall_score > 0.375:
-        overall_cat = "SOMEWHAT_NEGATIVE"
-    elif 0.375 >= agg_overall_score > 0.125:
-        overall_cat = "NEGATIVE"
-    else:
-        overall_cat = "VERY_NEGATIVE"
-
-    summed_emotion_scores = 0
-    for score in emotions.values():
-        summed_emotion_scores += score
-    for label, score in emotions.items():
-        emotions[label] = round(score / summed_emotion_scores, 4)
-
-    agg_toxicity_score = round(summed_toxicity / num_individuals, 4)
-    toxic_label = ""
-    if agg_toxicity_score < 0.25:
-        toxic_label = "Non-toxic"
-    elif 0.25 <= agg_toxicity_score <= 0.75:
-        toxic_label = "Neutral"
-    else:
-        toxic_label = "Toxic"
-
-    agg_positive_ratio = round(summed_positive_ratio / num_individuals, 4)
-    agg_neutral_ratio = round(summed_neutral_ratio / num_individuals, 4)
-    agg_negative_ratio = round(summed_negative_ratio / num_individuals, 4)
-
-    return {
-        "overall": {
-            "general": {"category": overall_cat, "score": agg_overall_score},
-            "emotions": emotions,
-            "toxicity": {"level_of_toxic": toxic_label, "score": agg_toxicity_score},
-            "ratios": {
-                "positive": agg_positive_ratio,
-                "neutral": agg_neutral_ratio,
-                "negative": agg_negative_ratio,
-            },
-        },
-        "individual_data": sentiment_data,
-    }
-
-
 def process_new_data(new_data: list):
     scores = []
     for item in new_data:
@@ -251,4 +164,4 @@ def process_sentiment_records(source_id):
     scores = []
     for d in data:
         scores.append(analyse_content(d))
-    return aggregate_sentiment_data(scores)
+    return aggregation.aggregate_sentiment_data(scores)
--- backend/engine/engine/settings.py
+++ backend/engine/engine/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-m)!b_a)sc(p6i0r)6hniwi4dxki+l1!qq@v&cera7sq4y7sv(6'
+SECRET_KEY = "django-insecure-m)!b_a)sc(p6i0r)6hniwi4dxki+l1!qq@v&cera7sq4y7sv(6"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -28,62 +28,61 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 CORS_ORIGIN_ALLOW_ALL = False
-CORS_ORIGIN_WHITELIST = (
-       'http://localhost:4200',
-)
+CORS_ORIGIN_WHITELIST = ("http://localhost:4200",)
 
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'analyser',
-    'corsheaders',
+    "django.contrib.admin",
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
+    "django.contrib.staticfiles",
+    "analyser",
+    "aggregator",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
 ]
 
-ROOT_URLCONF = 'engine.urls'
+ROOT_URLCONF = "engine.urls"
 
 TEMPLATES = [
     {
-        'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
-        'APP_DIRS': True,
-        'OPTIONS': {
-            'context_processors': [
-                'django.template.context_processors.debug',
-                'django.template.context_processors.request',
-                'django.contrib.auth.context_processors.auth',
-                'django.contrib.messages.context_processors.messages',
+        "BACKEND": "django.template.backends.django.DjangoTemplates",
+        "DIRS": [],
+        "APP_DIRS": True,
+        "OPTIONS": {
+            "context_processors": [
+                "django.template.context_processors.debug",
+                "django.template.context_processors.request",
+                "django.contrib.auth.context_processors.auth",
+                "django.contrib.messages.context_processors.messages",
             ],
         },
     },
 ]
 
-WSGI_APPLICATION = 'engine.wsgi.application'
+WSGI_APPLICATION = "engine.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
@@ -93,16 +92,16 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
     },
 ]
 
@@ -110,9 +109,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
@@ -122,9 +121,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = "static/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
