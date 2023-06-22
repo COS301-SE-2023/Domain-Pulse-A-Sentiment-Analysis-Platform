2f5057fd6688e0735c9a6d3e8bae36e38ab068b9
Luke Lawson <u21433811@tuks.co.za>
2023-06-22 19:28:55 +0200

    🎨 (backend) Boilerplate for aggregator tests

--- backend/engine/aggregator/tests.py
+++ backend/engine/aggregator/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from utils import mock_data
+import mock
 
 # Create your tests here.
+
+
+class DataAggregationTests(TestCase):
+    # -------------------------- UNIT TESTS --------------------------
+    def test_aggregate_sentiment_data_unit(self):
+        pass
+
+    # ----------------------------------------------------------------
+
+    # ---------------------- INTEGRATION TESTS -----------------------
+
+    # ----------------------------------------------------------------
