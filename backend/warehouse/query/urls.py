18fb7b25dc0a85c0934332d92e2d217a181a1783
Luke Lawson <u21433811@tuks.co.za>
2023-06-23 12:41:45 +0200

    🚧 (backend) Added endpoints for query engine

--- backend/warehouse/query/urls.py
+++ backend/warehouse/query/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("hello/", views.say_hello)
+    path("hello", views.say_hello),
+    path("get_source_dashboard/", views.get_dashboard_data_source),
+    path("get_domain_dashboard/", views.get_dashboard_data_domain),
+    path("refresh_source", views.refresh_source),
 ]
--- backend/warehouse/query/views.py
+++ backend/warehouse/query/views.py
@@ -9,27 +9,24 @@ def say_hello(request):
 
 
 def get_dashboard_data_source(request: HttpRequest):
-    pass
-
     # 1. Query the sentiment records database for all data from a provided source
     # 2. Determine the meta-data
     # 3. Send that data to the aggregator (engine) to get cumulative metrics
     # 4. Return data from the response from 3, as well as meta-data and an other status codes, etc
+    return JsonResponse({"message": "hello there!"})
 
 
 def get_dashboard_data_domain(request: HttpRequest):
-    pass
-
     # 1. Query the sentiment records database for all data from a provided domain
     # 2. Determine the meta-data
     # 3. Send that data to the aggregator (engine) to get cumulative metrics
     # 4. Return data from the response from 3, as well as meta-data and an other status codes, etc
+    return JsonResponse({"message": "hello there!"})
 
 
 def refresh_source(request: HttpRequest):
-    pass
-
     # 1. Given the appropriate source, invoke the SourceConnector to get new data (ie: past the most recent timestamp)
     # 2. Send the new data from 1 to the analyser (engine)
     # 3. Store the data with its computed metrics in the database
     # 4. (Frontend concern) - call the get_data_dashboard_source/domain endpoint to refresh the frontend dashboard
+    return JsonResponse({"message": "hello there!"})
