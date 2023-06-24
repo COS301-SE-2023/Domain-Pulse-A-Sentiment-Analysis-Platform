b01b0767f6c4a36a2786d59b569400a739f38764
Caden Dobson <106334873+cadendobson@users.noreply.github.com>
2023-06-24 12:15:56 +0200

    ✨ (frontend) added routing to application

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -13,66 +13,8 @@ import {
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass'],
-  animations: [
-    trigger('collapsed', [
-      state(
-        'collapsed',
-        style({
-          width: '5%',
-        })
-      ),
-      state(
-        'notCollapsed',
-        style({
-          width: '20%',
-        })
-      ),
-      transition(
-        'collapsed <=> notCollapsed',
-        group([
-          query('@logoSwitch', [animateChild()]),
-          animate('0.3s ease-in-out'),
-        ])
-      ),
-    ]),
-  ],
+  styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
-  sidebarCollapsed = true;
-  title = 'Domain Pulse';
 
-  showAddSourcesModal = false;
-  selectedDomain$ = this.backendService.selectedDomain$;
-
-  newSouceName = '';
-  newSourcePlatform = '';
-
-  constructor(private backendService: BackendService) {}
-
-  addNewSource() {
-    console.log('platform: ' + this.newSourcePlatform);
-    this.backendService.addNewSource(this.newSouceName, this.newSourcePlatform);
-    this.newSouceName = '';
-
-    this.toggleAddSourcesModal();
-  }
-
-  toggleAddSourcesModal() {
-    if (!this.showAddSourcesModal) {
-      this.showAddSourcesModal = true;
-    } else {
-      this.showAddSourcesModal = false;
-    }
-  }
-
-  openSidebar() {
-    this.sidebarCollapsed = false;
-  }
-
-  colapseSidebar() {
-    setTimeout(() => {
-      this.sidebarCollapsed = true;
-    }, 300);
-  }
 }
