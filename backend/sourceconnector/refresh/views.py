f943e431f58ffc3ec477134d35385f088672fe09
Luke Lawson <u21433811@tuks.co.za>
2023-06-24 12:26:05 +0200

    ✨ (backend) Source connector mocked out

--- backend/sourceconnector/refresh/views.py
+++ backend/sourceconnector/refresh/views.py
@@ -6,8 +6,28 @@ from utils import mock_data
 
 
 def refresh_instagram_source(request: HttpRequest, source_id):
-    return JsonResponse({"it": "worked!"})
+    source_id = str(source_id)
+
+    retData = []
+
+    if source_id == "2":
+        retData = mock_data.MOCK_DATA["2"]
+    elif source_id == "3":
+        retData = mock_data.MOCK_DATA["3"]
+
+    return JsonResponse({"new_data": retData})
 
 
 def refresh_google_reviews_source(request: HttpRequest, source_id):
-    return JsonResponse({"it": "worked!"})
+    source_id = str(source_id)
+
+    retData = []
+
+    if source_id == "0":
+        retData = mock_data.MOCK_DATA["0"]
+    elif source_id == "1":
+        retData = mock_data.MOCK_DATA["1"]
+    elif source_id == "4":
+        retData = mock_data.MOCK_DATA["4"]
+
+    return JsonResponse({"new_data": retData})
