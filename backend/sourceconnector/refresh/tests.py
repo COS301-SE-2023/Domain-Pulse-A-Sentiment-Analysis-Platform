d35f84752380c212e5fe9a247038b9dd73f24a98
Luke Lawson <u21433811@tuks.co.za>
2023-06-25 16:37:29 +0200

    ✅ (backend) Added remaining tests for source connector

--- backend/sourceconnector/refresh/tests.py
+++ backend/sourceconnector/refresh/tests.py
@@ -26,4 +26,28 @@ class RefreshInstagramSourceTestCase(TestCase):
         expected_data = {"new_data": mock_data.MOCK_DATA["3"]}
         self.assertEqual(response.content, JsonResponse(expected_data).content)
 
+    def test_refresh_googlereviews_source_with_source_id_0(self):
+        response = self.client.get("/refresh/googlereviews/0/")
+
+        self.assertEqual(response.status_code, 200)
+
+        expected_data = {"new_data": mock_data.MOCK_DATA["0"]}
+        self.assertEqual(response.content, JsonResponse(expected_data).content)
+
+    def test_refresh_googlereviews_source_with_source_id_1(self):
+        response = self.client.get("/refresh/googlereviews/1/")
+
+        self.assertEqual(response.status_code, 200)
+
+        expected_data = {"new_data": mock_data.MOCK_DATA["1"]}
+        self.assertEqual(response.content, JsonResponse(expected_data).content)
+
+    def test_refresh_googlereviews_source_with_source_id_4(self):
+        response = self.client.get("/refresh/googlereviews/4/")
+
+        self.assertEqual(response.status_code, 200)
+
+        expected_data = {"new_data": mock_data.MOCK_DATA["4"]}
+        self.assertEqual(response.content, JsonResponse(expected_data).content)
+
     # ---------------------------------------------------
