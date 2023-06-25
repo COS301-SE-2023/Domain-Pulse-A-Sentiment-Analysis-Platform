b9f569dbbcbfe3f789c087e47c8ab41cbde9f331
Thomas <thomasblendulf789@gmail.com>
2023-06-25 18:37:14 +0200

    ✅ (backend) Created More tests for profiles

--- backend/profiles/profileservice/tests.py
+++ backend/profiles/profileservice/tests.py
@@ -55,7 +55,83 @@ class ProfilesTests(TestCase):
             )
         else:
             assert (False)
+
+    def test_edit_profile_picture(self):
+        class MockUser:
+            is_authenticated = True
+
+        user = profilescrud.create_user("test","t@test.com","test")
+        rf = RequestFactory()
+        testPictureURL="test.com"
+        data={'id': user["id"], "pictureURL": testPictureURL}
+        post_request = rf.post('/profiles/edit_profile_picture', data, content_type='application/json')
+        post_request.user = MockUser()
+        result=json.loads(profile_views.edit_profile_picture(post_request).content)
+        if result["status"] == "SUCCESS":
+            assert (result["id"] == user["id"]
+                and result["profileIcon"] == testPictureURL
+            )
+        else:
+            assert (False)
         
+    def test_edit_profile_mode(self):
+        class MockUser:
+            is_authenticated = True
+
+        user = profilescrud.create_user("test","t@test.com","test")
+        rf = RequestFactory()
+        testMode=True
+        data={'id': user["id"], "mode":testMode }
+        post_request = rf.post('/profiles/edit_profile_mode', data, content_type='application/json')
+        post_request.user = MockUser()
+        result=json.loads(profile_views.edit_profile_mode(post_request).content)
+        if result["status"] == "SUCCESS":
+            assert (result["id"] == user["id"]
+                and result["mode"] ==testMode
+            )
+        else:
+            assert (False)
+
+    def test_add_domain_to_profile(self):
+        class MockUser:
+            is_authenticated = True
+
+        user = profilescrud.create_user("test","t@test.com","test")
+        rf = RequestFactory()
+        testDomainID=3
+        data={'id': user["id"], "domain_id":testDomainID }
+        post_request = rf.post('/profiles/add_domain_to_profile', data, content_type='application/json')
+        post_request.user = MockUser()
+        result=json.loads(profile_views.add_domain_to_profile(post_request).content)
+        if result["status"] == "SUCCESS":
+            assert (result["id"] == user["id"]
+                and testDomainID in result["domainIDs"]
+            )
+        else:
+            assert (False)
+
+    def test_remove_domain_from_profile(self):
+        class MockUser:
+            is_authenticated = True
+
+        user = profilescrud.create_user("test","t@test.com","test")
+        rf = RequestFactory()
+
+        testDomainID=3
+        setupData={'id': user["id"], "domain_id":testDomainID }
+        post_request = rf.post('/profiles/add_domain_to_profile', setupData, content_type='application/json')
+        post_request.user = MockUser()
+        profile_views.add_domain_to_profile(post_request)
+        data={'id': user["id"], "domain_id":testDomainID }
+        post_request = rf.post('/profiles/remove_domain_from_profile', data, content_type='application/json')
+        post_request.user = MockUser()
+        result=json.loads(profile_views.remove_domain_from_profile(post_request).content)
+        if result["status"] == "SUCCESS":
+            assert (result["id"] == user["id"]
+                and testDomainID not in result["domainIDs"]
+            )
+        else:
+            assert (False)
 
    
 
--- backend/profiles/utils/profilescrud.py
+++ backend/profiles/utils/profilescrud.py
@@ -4,37 +4,7 @@ from profileservice import models as profile_models
 
 LIGHT = False
 DARK = True
-ID_COUNTER = 10
 
-profiles_db = [
-    {
-        "id": 1,
-        "mode": LIGHT,
-        "profileIcon": "https://www.waterfront.co.za/wp-content/uploads/2018/05/mcdonalds.jpg",
-        "domainIDs": ["1", "2"],
-        "user_id":1,
-    },
-    {
-        "id": 2,
-        "mode": DARK,
-        "profileIcon": "https://pbs.twimg.com/profile_images/1655865005370163207/o1wfCDc3_400x400.jpg",
-        "domainIDs": ["3", "4"],
-        "user_id":2,
-    },
-    {
-        "id": 3,
-        "mode": LIGHT,
-        "profileIcon": "https://logos-world.net/wp-content/uploads/2022/12/Nandos-Logo.jpg",
-        "domainIDs": ["5"],
-        "user_id":3,
-    }
-]
-
-
-def next_id():
-    global ID_COUNTER
-    ID_COUNTER += 1
-    return ID_COUNTER
 
 
 def create_user(uname,email,pword):
@@ -47,7 +17,6 @@ def create_user(uname,email,pword):
 def create_profile(user_id, profileIcon, mode=LIGHT):
     
     # user_id = int(user_id)
-    id = next_id()
     profile=profile_models.Profiles.objects.create( id = user_id.id,
         mode=mode, profileIcon=profileIcon, userID=user_id, domainIDs=[])
     
@@ -71,7 +40,7 @@ def edit_profile_picture(request,id, pictureURL):
         profile= profile_models.Profiles.objects.get(id=id)
         profile.profileIcon=pictureURL 
         profile.save()
-        return {"id":profile.id,"mode":profile.mode,"profileIcon":profile.profileIcon,"domainIDs":profile.domainIDs,"userID":profile.userID_id}
+        return {"status":"SUCCESS","id":profile.id,"mode":profile.mode,"profileIcon":profile.profileIcon,"domainIDs":profile.domainIDs,"userID":profile.userID_id}
     else:
         return {"status":"FAILURE"}
 
@@ -82,7 +51,7 @@ def edit_profile_mode(request,id, mode):
         profile= profile_models.Profiles.objects.get(id=id)
         profile.mode= mode
         profile.save()
-        return {"id":profile.id,"mode":profile.mode,"profileIcon":profile.profileIcon,"domainIDs":profile.domainIDs,"userID":profile.userID_id}
+        return {"status":"SUCCESS","id":profile.id,"mode":profile.mode,"profileIcon":profile.profileIcon,"domainIDs":profile.domainIDs,"userID":profile.userID_id}
     else:
         return {"id":profile.id,"mode":profile.mode,"profileIcon":profile.profileIcon,"domainIDs":profile.domainIDs,"userID":profile.userID_id}
 
@@ -109,7 +78,7 @@ def add_domain_to_profile(request,id, domain_id):
         if domain_id not in profile.domainIDs:
             profile.domainIDs.append(domain_id)
             profile.save()
-        return {"id":profile.id,"mode":profile.mode,"profileIcon":profile.profileIcon,"domainIDs":profile.domainIDs,"userID":profile.userID_id}
+        return {"status":"SUCCESS","id":profile.id,"mode":profile.mode,"profileIcon":profile.profileIcon,"domainIDs":profile.domainIDs,"userID":profile.userID_id}
     else:
         return {"status":"FAILURE"}
 
@@ -122,7 +91,7 @@ def remove_domain_from_profile(request,id, domain_id):
         if domain_id in profile.domainIDs:
             profile.domainIDs.remove(domain_id)
             profile.save()
-        return {"id":profile.id,"mode":profile.mode,"profileIcon":profile.profileIcon,"domainIDs":profile.domainIDs,"userID":profile.userID_id}
+        return {"status":"SUCCESS","id":profile.id,"mode":profile.mode,"profileIcon":profile.profileIcon,"domainIDs":profile.domainIDs,"userID":profile.userID_id}
     else:
         return {"status":"FAILURE"}
 
@@ -131,7 +100,7 @@ def get_domains_for_user(request,id):
     if request.user.is_authenticated:
         id = int(id)
         profile= profile_models.Profiles.objects.get(id=id)
-        return {"id":profile.id, "domainIDs":profile.domainIDs}
+        return {"status":"SUCCESS","id":profile.id, "domainIDs":profile.domainIDs}
     else:
         return {"status":"FAILURE"}
 
@@ -140,7 +109,7 @@ def get_profile(request,id):
     if request.user.is_authenticated:
         id = int(id)
         profile= profile_models.Profiles.objects.get(id=id)
-        return {"id":profile.id,"mode":profile.mode,"profileIcon":profile.profileIcon,"domainIDs":profile.domainIDs,"userID":profile.userID_id}
+        return {"status":"SUCCESS","id":profile.id,"mode":profile.mode,"profileIcon":profile.profileIcon,"domainIDs":profile.domainIDs,"userID":profile.userID_id}
     else:
         return {"status":"FAILURE"}
 
@@ -175,7 +144,6 @@ def delete_user(request,username,password):
                 return {"status": "FAILURE"}
         else:
             return {"status": "FAILURE"}
-        return {"status": "SUCCESS"}
     else:
         return {"status": "FAILURE"}
 
