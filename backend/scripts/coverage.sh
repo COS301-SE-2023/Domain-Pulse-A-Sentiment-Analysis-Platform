6c7f5e06514b6c3c1986174393ebe0d78d2b51d2
Caden Dobson <106334873+cadendobson@users.noreply.github.com>
2023-06-27 04:53:56 +0200

    👷 (ci) trying to fix pipeline

--- backend/scripts/coverage.sh
+++ backend/scripts/coverage.sh
@@ -1,3 +1,19 @@
-cd engine
+project1="../backend/engine"
+project2="../backend/profilemanager"
+project3="../backend/warehouse"
+
+cd $project1
 coverage run --source='.' manage.py test
-coverage lcov 
+coverage report -m
+
+cd $project2
+coverage run --source='.' manage.py test
+coverage report -m
+
+cd $project3
+coverage run --source='.' manage.py test
+coverage report -m
+
+coverage combine
+coverage report -m
+coverage lcov -o coverage.lcov
