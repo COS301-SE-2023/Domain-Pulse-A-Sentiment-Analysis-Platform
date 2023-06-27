c3a75857037cb186dc320781796d59f7e2f0bc51
Caden Dobson <106334873+cadendobson@users.noreply.github.com>
2023-06-27 05:37:42 +0200

    test

--- backend/scripts/coverage.sh
+++ backend/scripts/coverage.sh
@@ -1,21 +1,24 @@
-project1="./engine"
-project2="../profilemanager"
-project3="../warehouse"
+project1="../backend/engine"
+project2="../backend/profilemanager"
+project3="../backend/warehouse"
 
 cd $project1
 coverage run --source='.' manage.py test
 coverage report -m
 coverage xml -o coverage.xml
+cd ..
 
 cd $project2
 coverage run --source='.' manage.py test
 coverage report -m
 coverage xml -o coverage.xml
+cd ..
 
 cd $project3
 coverage run --source='.' manage.py test
 coverage report -m
 coverage xml -o coverage.xml
+
 coverage combine
 coverage report -m
 coverage html -d coverage_html
