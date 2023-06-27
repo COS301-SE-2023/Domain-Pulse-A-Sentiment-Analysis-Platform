c75842b91a23e27c5206b79bdece246f197bb772
Caden Dobson <106334873+cadendobson@users.noreply.github.com>
2023-06-27 05:07:33 +0200

    👷 (pipeline potential fix)

--- backend/scripts/coverage.sh
+++ backend/scripts/coverage.sh
@@ -5,15 +5,16 @@ project3="../backend/warehouse"
 cd $project1
 coverage run --source='.' manage.py test
 coverage report -m
-
+coverage xml -o coverage.xml
 cd $project2
 coverage run --source='.' manage.py test
 coverage report -m
-
+coverage xml -o coverage.xml
 cd $project3
 coverage run --source='.' manage.py test
 coverage report -m
-
+coverage xml -o coverage.xml
 coverage combine
 coverage report -m
+coverage html -d coverage_html
 coverage lcov -o coverage.lcov
