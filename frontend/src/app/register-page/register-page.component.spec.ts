98104cf6c910759f8a63d5c1b810bc9db48f2918
Thuthuka Khumalo <thuthuka111@gmail.com>
2023-07-04 18:57:55 +0200

    ✅ (frontend) login and register page tests

--- frontend/src/app/register-page/register-page.component.spec.ts
+++ frontend/src/app/register-page/register-page.component.spec.ts
@@ -1,23 +1,41 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 
 import { RegisterPageComponent } from './register-page.component';
+import { Actions, NgxsModule, Store, ofActionDispatched } from '@ngxs/store';
+import { AppApi } from '../app.api';
+import { FormsModule } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { RegisterUser } from '../app.actions';
 
 describe('RegisterPageComponent', () => {
   let component: RegisterPageComponent;
-  let fixture: ComponentFixture<RegisterPageComponent>;
+  let storeSpy: jasmine.SpyObj<Store>;
+  let appApiSpy: jasmine.SpyObj<AppApi>;
+  let actions$: Observable<any>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ RegisterPageComponent ]
-    })
-    .compileComponents();
+  beforeEach(() => {
+    appApiSpy = jasmine.createSpyObj('AppApi', ['registerUser']);
+    appApiSpy.registerUser.and.callThrough();
 
-    fixture = TestBed.createComponent(RegisterPageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
+    TestBed.configureTestingModule({
+      providers: [
+        RegisterPageComponent,
+        { provide: AppApi, useValue: appApiSpy },
+      ],
+      imports: [NgxsModule.forRoot([]), FormsModule],
+    });
+
+    component = TestBed.inject(RegisterPageComponent);
+    storeSpy = TestBed.inject(Store) as jasmine.SpyObj<Store>;
+    actions$ = TestBed.inject(Actions);
+    // spyOn(storeSpy, 'dispatch').and.callThrough();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it('fire "RegisterUser" action when register function called', (done) => {
+    actions$.pipe(ofActionDispatched(RegisterUser)).subscribe((_) => {
+      done();
+    });
+
+    component.register();
   });
 });
