17c705e4893a8ed4110038bca9dbf43d67b95873
Thuthuka Khumalo <thuthuka111@gmail.com>
2023-07-04 11:47:29 +0200

    🔨 (backend) dynamic port for engine using backend/.env

--- backend/engine/engine/settings.py
+++ backend/engine/engine/settings.py
@@ -9,12 +9,18 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
+from dotenv import load_dotenv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# ENV DIR is 2 parent directories up from BASE_DIR
+ENV_FILE = BASE_DIR.parent / '.env'
+load_dotenv(ENV_FILE)
+
+RUNSERVER_PORT = os.getenv("DJANGO_ENGINE_PORT")
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
--- backend/engine/manage.py
+++ backend/engine/manage.py
@@ -2,7 +2,8 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-
+from django.core.management.commands.runserver import Command as runserver
+from django.conf import settings
 
 def main():
     """Run administrative tasks."""
@@ -15,8 +16,8 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+    runserver.default_port = settings.RUNSERVER_PORT
     execute_from_command_line(sys.argv)
-
 
 if __name__ == '__main__':
     main()
