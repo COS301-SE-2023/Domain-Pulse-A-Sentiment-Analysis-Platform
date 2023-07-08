7d60fb02ecfca8e73102b2361bf8cc6c9fdc2da0
Thomas <thomasblendulf789@gmail.com>
2023-07-09 00:48:23 +0200

    ✨ (backend) Continued with youtube API

--- backend/sourceconnector/utils/youtubeAPI.py
+++ backend/sourceconnector/utils/youtubeAPI.py
@@ -2,7 +2,7 @@ import requests
 import os
 from pathlib import Path
 from dotenv import load_dotenv
-
+import json
 BASE_DIR = Path(__file__).resolve().parent.parent
 ENV_FILE = BASE_DIR.parent / '.env'
 load_dotenv(ENV_FILE)
@@ -10,15 +10,26 @@ load_dotenv(ENV_FILE)
 API_KEY = os.getenv("YOUTUBE_API_KEY")
 
 
-def get_channel(link):
+def get_channel_playlist(link):
     split_url = link.split("/")
+    response=None
     if split_url[0] == "https":
         if split_url[2][0]=="@":
                 response =requests.get('https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forUsername='+str( split_url[2][1:])+'&key='+str(API_KEY))
         else:
-                response =requests.get('https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forUsername='+str( split_url[2])+'&key='+str(API_KEY))
+                response =requests.get('https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&id='+str( split_url[2])+'&key='+str(API_KEY))
     elif split_url[0]=="youtube.com":
         if split_url[1][0]=="@":
             response =requests.get('https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forUsername='+str( split_url[1][1:])+'&key='+str(API_KEY))
         else:
-            response =requests.get('https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&forUsername='+str( split_url[1])+'&key='+str(API_KEY))
+            response =requests.get('https://youtube.googleapis.com/youtube/v3/channels?part=contentDetails&id='+str( split_url[1])+'&key='+str(API_KEY))
+    if response==None:
+         return {"status":"FAILURE"}
+    else:
+         data = response.text
+         final_data=json.loads(data)
+         return final_data["items"]["contentDetails"]["relatedPlaylists"]["uploads"]
+
+def get_playlist_video(playlist_id):
+    response = requests.get("https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails&playlistId="+str(playlist_id)+"&key="+str(API_KEY))
+    return response
