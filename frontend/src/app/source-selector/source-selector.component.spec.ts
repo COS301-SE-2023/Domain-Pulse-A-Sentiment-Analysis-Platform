d40b04fc1cd0ceea8df6a5fd37784ec2db74abe0
Caden Dobson <106334873+cadendobson@users.noreply.github.com>
2023-07-10 15:39:06 +0200

    ✅ (frontend) setup testing for source-selector - test selectSourceFunc

--- frontend/src/app/source-selector/source-selector.component.spec.ts
+++ frontend/src/app/source-selector/source-selector.component.spec.ts
@@ -1,23 +1,49 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { TestBed } from '@angular/core/testing';
 
 import { SourceSelectorComponent } from './source-selector.component';
+import { Actions, NgxsModule, Store, ofActionDispatched } from '@ngxs/store';
+import { AppApi } from '../app.api';
+import { Observable } from 'rxjs';
+import { FormsModule } from '@angular/forms';
+import { AttempPsswdLogin, SetSource } from '../app.actions';
+import { DisplaySource } from '../app.state';
 
-// describe('SourceSelectorComponent', () => {
-//   let component: SourceSelectorComponent;
-//   let fixture: ComponentFixture<SourceSelectorComponent>;
+describe('SourceSelectorComponent', () => {
+  let component: SourceSelectorComponent;
+  let storeSpy: jasmine.SpyObj<Store>;
+  let appApiSpy: jasmine.SpyObj<AppApi>;
+  let actions$: Observable<any>;
 
-//   beforeEach(async () => {
-//     await TestBed.configureTestingModule({
-//       declarations: [ SourceSelectorComponent ]
-//     })
-//     .compileComponents();
+  beforeEach(() => {
+    appApiSpy = jasmine.createSpyObj('AppApi', ['getSourceSentimentData']);
+    appApiSpy.getSourceSentimentData.and.callThrough();
 
-//     fixture = TestBed.createComponent(SourceSelectorComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+    // appStateSpy = jasmine.createSpyObj('AppState', ['setSource']);
+    // appStateSpy.setSource.and.callThrough();
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+    TestBed.configureTestingModule({
+      providers: [SourceSelectorComponent, { provide: AppApi, useValue: appApiSpy }],
+      imports: [NgxsModule.forRoot([]), FormsModule],
+    });
+
+    component = TestBed.inject(SourceSelectorComponent);
+    storeSpy = TestBed.inject(Store) as jasmine.SpyObj<Store>;
+    actions$ = TestBed.inject(Actions);
+  });
+
+  it('should fire the "SetSource" action when selectSource function called', (done) => {
+    actions$.pipe(ofActionDispatched(SetSource)).subscribe((_) => {
+      expect(true).toBe(true);
+      done();
+    });
+
+    const dummyDisplaySource: DisplaySource = {
+      id: 1,
+      name: 'test',
+      url: 'test',
+      selected: true,
+    };
+
+    component.selectSource(dummyDisplaySource);
+  });
+});
