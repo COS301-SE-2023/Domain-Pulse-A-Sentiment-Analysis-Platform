0b9d5486140460cdf8c28dea31ebea2c29d1e471
Caden Dobson <106334873+cadendobson@users.noreply.github.com>
2023-07-10 17:24:46 +0200

    ✅ (frontend) 100% tests on source selector comp

--- frontend/src/app/source-selector/source-selector.component.spec.ts
+++ frontend/src/app/source-selector/source-selector.component.spec.ts
@@ -5,45 +5,70 @@ import { Actions, NgxsModule, Store, ofActionDispatched } from '@ngxs/store';
 import { AppApi } from '../app.api';
 import { Observable } from 'rxjs';
 import { FormsModule } from '@angular/forms';
-import { AttempPsswdLogin, SetSource } from '../app.actions';
+import { AddNewSource, AttempPsswdLogin, SetSource } from '../app.actions';
 import { DisplaySource } from '../app.state';
 
 describe('SourceSelectorComponent', () => {
-  let component: SourceSelectorComponent;
-  let storeSpy: jasmine.SpyObj<Store>;
-  let appApiSpy: jasmine.SpyObj<AppApi>;
-  let actions$: Observable<any>;
+	let component: SourceSelectorComponent;
+	let storeSpy: jasmine.SpyObj<Store>;
+	let appApiSpy: jasmine.SpyObj<AppApi>;
+	let actions$: Observable<any>;
 
-  beforeEach(() => {
-    appApiSpy = jasmine.createSpyObj('AppApi', ['getSourceSentimentData']);
-    appApiSpy.getSourceSentimentData.and.callThrough();
+	beforeEach(() => {
+		appApiSpy = jasmine.createSpyObj('AppApi', ['getSourceSentimentData']);
+		appApiSpy.getSourceSentimentData.and.callThrough();
 
-    // appStateSpy = jasmine.createSpyObj('AppState', ['setSource']);
-    // appStateSpy.setSource.and.callThrough();
+		// appStateSpy = jasmine.createSpyObj('AppState', ['setSource']);
+		// appStateSpy.setSource.and.callThrough();
 
-    TestBed.configureTestingModule({
-      providers: [SourceSelectorComponent, { provide: AppApi, useValue: appApiSpy }],
-      imports: [NgxsModule.forRoot([]), FormsModule],
-    });
+		TestBed.configureTestingModule({
+			providers: [SourceSelectorComponent, { provide: AppApi, useValue: appApiSpy }],
+			imports: [NgxsModule.forRoot([]), FormsModule],
+		});
 
-    component = TestBed.inject(SourceSelectorComponent);
-    storeSpy = TestBed.inject(Store) as jasmine.SpyObj<Store>;
-    actions$ = TestBed.inject(Actions);
-  });
+		component = TestBed.inject(SourceSelectorComponent);
+		storeSpy = TestBed.inject(Store) as jasmine.SpyObj<Store>;
+		actions$ = TestBed.inject(Actions);
+	});
 
-  it('should fire the "SetSource" action when selectSource function called', (done) => {
-    actions$.pipe(ofActionDispatched(SetSource)).subscribe((_) => {
-      expect(true).toBe(true);
-      done();
-    });
+	it('should fire a "AddNewSource" action', (done: DoneFn) => {
+		component.newSouceName = 'New Domain Name';
 
-    const dummyDisplaySource: DisplaySource = {
-      id: 1,
-      name: 'test',
-      url: 'test',
-      selected: true,
-    };
+		actions$.pipe(ofActionDispatched(AddNewSource)).subscribe(() => {
+			// expect the clearing of the set variables
+			setTimeout(() => {
+				expect(component.newSouceName).toBe('');
 
-    component.selectSource(dummyDisplaySource);
-  });
+				done();
+			}, 300);
+		});
+
+		component.addNewSource();
+	});
+
+	it('should fire the "SetSource" action when selectSource function called', (done) => {
+		actions$.pipe(ofActionDispatched(SetSource)).subscribe((_) => {
+			expect(true).toBe(true);
+			done();
+		});
+
+		const dummyDisplaySource: DisplaySource = {
+			id: 1,
+			name: 'test',
+			url: 'test',
+			selected: true,
+		};
+
+		component.selectSource(dummyDisplaySource);
+	});
+
+	it('should toggle the add source modal', () => {
+		component.showAddSourcesModal = false;
+		component.toggleAddSourcesModal();
+		expect(component.showAddSourcesModal).toBe(true);
+
+		component.showAddSourcesModal = true;
+		component.toggleAddSourcesModal();
+		expect(component.showAddSourcesModal).toBe(false);
+	});
 });
