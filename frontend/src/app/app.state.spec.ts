6282f434ae3fd50c8fa526fb47e0d7bbc43f080c
Caden Dobson <106334873+cadendobson@users.noreply.github.com>
2023-07-14 13:38:54 +0200

    ✅ (frontend) added more app state tests

--- frontend/src/app/app.state.spec.ts
+++ frontend/src/app/app.state.spec.ts
@@ -1,27 +1,33 @@
 import { TestBed } from '@angular/core/testing';
-import { NgxsModule, Store } from '@ngxs/store';
+import { Actions, NgxsModule, Store, ofActionDispatched } from '@ngxs/store';
 import { AppState } from './app.state';
 import {
+  AttempPsswdLogin,
   CheckAuthenticate,
   ChooseStatistic,
+  GetDomains,
   RegisterUser,
+  SetProfileId,
 } from './app.actions';
 import { AppApi } from './app.api';
-import { of } from 'rxjs';
+import { Observable, of, zip } from 'rxjs';
 
 describe('AppState', () => {
   let store: Store;
   let apiSpy: jasmine.SpyObj<AppApi>;
+  let actions$: Observable<any>;
 
   beforeEach(async () => {
     apiSpy = jasmine.createSpyObj('AppApi', [
       'registerUser',
       'getDomainIDs',
       'checkAuthenticate',
+      'attemptPsswdLogin',
     ]);
-    apiSpy.getDomainIDs.and.callThrough();
+    apiSpy.getDomainIDs.and.returnValue(of({ status: 'SUCCESS', domainIDs: [] }));
     apiSpy.registerUser.and.returnValue(of({ status: 'SUCCESS' }));
     apiSpy.checkAuthenticate.and.returnValue(of({ status: 'SUCCESS' }));
+    apiSpy.attemptPsswdLogin.and.returnValue(of({ status: 'SUCCESS' }));
 
     await TestBed.configureTestingModule({
       imports: [NgxsModule.forRoot([AppState])],
@@ -30,6 +36,18 @@ describe('AppState', () => {
     }).compileComponents();
 
     store = TestBed.inject(Store);
+    actions$ = TestBed.inject(Actions);
+  });
+
+  it('set the correct dashboard info if there is a source that is selected', () => {
+    // dispatch event
+
+    // make sure overallSentiment and sampleData is set not set
+
+    // setup the selected source
+
+    // dispatch event
+    // make sure overallSentiment and sampleData is set
   });
 
   it('Should Set the userID when when the user has been authenticated', () => {
@@ -39,6 +57,18 @@ describe('AppState', () => {
 
     store.dispatch(new CheckAuthenticate());
     // check that the userid in local storage is set if the user is indeed authenticated
+  });
+
+  it('React correctly "AttempPsswdLogin" event', (done: DoneFn) => {
+    zip(
+      actions$.pipe(ofActionDispatched(SetProfileId)),
+      actions$.pipe(ofActionDispatched(GetDomains))
+    ).subscribe((_) => {
+      expect(apiSpy.attemptPsswdLogin).toHaveBeenCalled();
+      done();
+    });
+
+    store.dispatch(new AttempPsswdLogin('test_username', 'test_password'));
   });
 
   it('React correctly to registering user', () => {
