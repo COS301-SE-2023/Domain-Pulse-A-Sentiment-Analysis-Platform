aa0e4ee2bf302e135ef07bfaaa0713bf7c25c4be
Luke Lawson <u21433811@tuks.co.za>
2023-07-14 10:15:08 +0200

    🚧 (backend) Work on Youtube source

--- backend/sourceconnector/youtube/youtube_connector.py
+++ backend/sourceconnector/youtube/youtube_connector.py
@@ -1,21 +1,54 @@
 import requests
+from datetime import datetime
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+import datetime
+from django.http import JsonResponse, HttpRequest, HttpResponse
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+ENV_FILE = BASE_DIR.parent / ".env"
+load_dotenv(ENV_FILE)
+YOUTUBE_API_KEY = os.getenv("YOUTUBE_API_KEY")
 
 
-def get_comments_by_video_id(video_id: str):
+def get_comments_by_video_id(video_id: str, last_refresh_time):
     URL = f"https://www.googleapis.com/youtube/v3/commentThreads?part=snippet&videoId={video_id}&maxResults=100&key={YOUTUBE_API_KEY}"
+
     response = requests.get(url=URL)
     data = response.json()
+
+    comments = []
+
+    latest_retrieval = 0
 
     if "items" in data:
         for item in data["items"]:
             snippet = item["snippet"]["topLevelComment"]["snippet"]
             original_text = snippet["textOriginal"]
             last_updated_time = snippet["updatedAt"]
-            print("Original Text:", original_text)
-            print("Last Updated Time:", last_updated_time)
-    else:
-        print("No comments found.")
+            datetime_object = datetime.strptime(last_updated_time, "%Y-%m-%dT%H:%M:%SZ")
+            last_updated_timestamp = datetime_object.timestamp()
+
+            if last_updated_timestamp > last_refresh_time:
+                comments.append(
+                    {"text": original_text, "timestamp": last_updated_timestamp}
+                )
+
+                if last_updated_timestamp > latest_retrieval:
+                    latest_retrieval = last_updated_timestamp
+
+    return comments, latest_retrieval
 
 
 def handle_request(params):
-    pass
+    video_id = params["video_id"]
+    last_refresh_timestamp = float(params["last_refresh_timestamp"])
+
+    comments, latest_retrieval = get_comments_by_video_id(
+        video_id, last_refresh_timestamp
+    )
+
+    return JsonResponse(
+        {"status": "SUCCESS", "newdata": comments, "latest_retrieval": latest_retrieval}
+    )
