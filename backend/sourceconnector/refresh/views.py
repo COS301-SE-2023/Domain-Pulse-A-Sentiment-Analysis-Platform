1452970e9fc431dc6c7a6a8389696004525fb5f7
Luke Lawson <u21433811@tuks.co.za>
2023-07-14 09:21:00 +0200

    🚧 (backend) Views boilerplate

--- backend/sourceconnector/refresh/views.py
+++ backend/sourceconnector/refresh/views.py
@@ -1,17 +1,26 @@
 from django.shortcuts import render
 from django.http import JsonResponse, HttpRequest, HttpResponse
-from utils import mock_data
+import json
 
 # Create your views here.
 
 
 def refresh_youtube_video(request: HttpRequest, source_id):
-    return JsonResponse({"new_data": "test"})
+    if request.method == "POST":
+        raw_data = json.loads(request.body)
+
+    return JsonResponse({"status": "FAILURE", "details": "Invalid request"})
 
 
 def refresh_google_reviews(request: HttpRequest, source_id):
-    return JsonResponse({"new_data": "test"})
+    if request.method == "POST":
+        raw_data = json.loads(request.body)
+
+    return JsonResponse({"status": "FAILURE", "details": "Invalid request"})
 
 
 def refresh_tripadvisor(request: HttpRequest, source_id):
-    return JsonResponse({"new_data": "test"})
+    if request.method == "POST":
+        raw_data = json.loads(request.body)
+
+    return JsonResponse({"status": "FAILURE", "details": "Invalid request"})
