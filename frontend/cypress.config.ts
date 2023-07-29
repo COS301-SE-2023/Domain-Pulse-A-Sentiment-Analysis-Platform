8ad55ed97138d681f54a2a8459061fffaf5738d1
Thuthuka Khumalo <thuthuka111@gmail.com>
2023-07-29 16:47:50 +0200

    🚧 (e2e) initialize e2e framework

---
+++ frontend/cypress.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  
+  e2e: {
+    // 'baseUrl': 'http://localhost:4200'
+    'baseUrl': 'http://dev.dp.cos301.thuthuka.me'
+  },
+  
+  
+})
---
+++ frontend/cypress/e2e/login.cy.ts
@@ -0,0 +1,46 @@
+/// <reference types="cypress" />
+
+describe('Logging In - Basic Auth', function () {
+  // we can use these values to log in
+  const username = 'thugger';
+  const password = '123456';
+  let JWT: string = '';
+
+  before(() => {
+    cy.request('POST', '/api/profiles/profiles/login_user', {
+      username: username,
+      password: password,
+    })
+      .its('body')
+      .then((res) => {
+        JWT = res.JWT;
+      });
+  });
+
+  // but set the user before visiting the page
+  // so the app thinks it is already authenticated
+  beforeEach(function setUser() {
+    cy.visit('/', {
+      onBeforeLoad(win) {
+        // and before the page finishes loading
+        // set the user object in local storage
+        win.localStorage.setItem('JWT', JWT);
+      },
+    });
+    // the page should be opened and the user should be logged in
+  });
+
+  const inDashboard = () => {
+    cy.location('href').should('eq', '/');
+  };
+
+  it('redirects to /login', () => {
+    cy.visit('/');
+    cy.location('href').should('match', /login$/);
+  });
+
+  it('be in dashboard', () => {
+    cy.visit('/');
+    inDashboard();
+  });
+});
