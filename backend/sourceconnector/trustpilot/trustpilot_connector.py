a19621c58646aed84751b8eacf22a57571adbeef
Luke Lawson <u21433811@tuks.co.za>
2023-08-09 11:34:33 +0200

    ✨ (backend) Added TP module to SConn

---
+++ backend/sourceconnector/trustpilot/trustpilot_connector.py
@@ -0,0 +1,55 @@
+import requests
+from datetime import datetime
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+from django.http import JsonResponse, HttpRequest, HttpResponse
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+ENV_FILE = BASE_DIR.parent / ".env"
+load_dotenv(ENV_FILE)
+TRUSTPILOT_API_KEY = os.getenv("TRUSTPILOT_API_KEY")
+
+ENDPOINT = "https://api.app.outscraper.com/trustpilot/reviews"
+# url of the place that is reviewed - not the url of the TrustPilot page (performance). This url can be found as the last part of the TrustPilot URL
+# number of reviews to return (please keep this to a small number, around 1-10 ideally)
+REVIEWS_LIMIT = 5
+
+HEADERS = {"X-API-KEY": TRUSTPILOT_API_KEY}
+
+
+def call_outscraper(query_url: str, last_refreshed_timestamp):
+    response = requests.get(
+        url=ENDPOINT
+        + "?"
+        + "query="
+        + query_url
+        + "&limit="
+        + str(REVIEWS_LIMIT)
+        + "&cutoff="
+        + str(last_refreshed_timestamp)
+        + "&async="
+        + "false",
+        headers=HEADERS,
+    )
+
+    return response.json()
+
+
+def get_trustpilot_reviews(query_url: str, last_refreshed_timestamp):
+    results = call_outscraper(query_url, last_refreshed_timestamp)
+
+    latest_retrieval = -1
+
+    ret_data = []
+    if results["status"] == "Success":
+        reviews = results["data"][0]
+        review_item = {}
+        for r in reviews:
+            review_item = {"text": r["review_text"], "timestamp": r["review_date"]}
+
+            if r["review_date"] > latest_retrieval:
+                latest_retrieval = r["review_date"]
+
+            ret_data.append(review_item)
+    return ret_data, latest_retrieval
