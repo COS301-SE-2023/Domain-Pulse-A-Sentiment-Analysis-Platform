640b2631b6c55e022e321fda973c4b0605a9c1b0
Luke Lawson <u21433811@tuks.co.za>
2023-08-11 10:24:35 +0200

    🐛 (backend) Url to nowhere issue

--- backend/warehouse/query/urls.py
+++ backend/warehouse/query/urls.py
@@ -5,5 +5,4 @@ urlpatterns = [
     path("get_source_dashboard/", views.get_dashboard_data_source),
     path("get_domain_dashboard/", views.get_dashboard_data_domain),
     path("refresh_source/", views.refresh_source),
-    path("cleanup_sources/", views.cleanup_sources),
 ]
