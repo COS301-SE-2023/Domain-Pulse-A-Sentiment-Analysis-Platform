58fcff1e62fe13c9b53345c75d83da5a3db46c00
Luke Lawson <u21433811@tuks.co.za>
2023-08-12 11:24:20 +0200

    ✨ (backend) Added endpoint for live ingestion

---
+++ backend/warehouse/ingest/views.py
@@ -0,0 +1,44 @@
+from django.shortcuts import render
+from django.http import JsonResponse, HttpRequest
+from django.views.decorators.csrf import csrf_exempt
+from datamanager import sentiment_record_model
+import json
+import requests
+from authchecker import auth_checks
+import os
+import bleach
+
+# Create your views here.
+
+
+def ingest_live_review(request: HttpRequest):
+    if request.method == "POST":
+        raw_data = json.loads(request.body)
+        source_id_raw = raw_data["source_id"]
+        review_text = bleach.clean(raw_data["review_text"])
+        timestamp = int(raw_data["timestamp"])
+
+        ANALYSER_ENDPOINT = (
+            f"http://localhost:{str(os.getenv('DJANGO_ENGINE_PORT'))}/analyser/compute/"
+        )
+        request_to_engine_body = {"data": review_text}
+        response_from_analyser = requests.post(
+            ANALYSER_ENDPOINT, data=json.dumps(request_to_engine_body)
+        )
+
+        if response_from_analyser.status_code == 200:
+            new_record = response_from_analyser.json()
+            new_record["timestamp"] = timestamp
+            new_record["source_id"] = source_id_raw
+
+            sentiment_record_model.add_record(new_record)
+
+            return JsonResponse(
+                {"status": "SUCCESS", "details": "Review ingested successfully!"}
+            )
+        else:
+            return JsonResponse(
+                {"status": "FAILURE", "details": "Error communicating with analyser"}
+            )
+
+    return JsonResponse({"status": "FAILURE", "details": "Invalid request"})
--- backend/warehouse/warehouse/urls.py
+++ backend/warehouse/warehouse/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('query/', include('query.urls'))
+    path("admin/", admin.site.urls),
+    path("query/", include("query.urls")),
+    path("ingest/", include("ingest.urls")),
 ]
