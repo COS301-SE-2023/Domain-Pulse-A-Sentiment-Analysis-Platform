d82021857b8aba208c62ad5158166bd78b60972d
Thomas <thomasblendulf789@gmail.com>
2023-08-13 22:57:01 +0200

    🚧 (backend) Cropping images better

--- backend/profiles/reportgenerator/views.py
+++ backend/profiles/reportgenerator/views.py
@@ -5,51 +5,86 @@ from django.http import JsonResponse, HttpRequest, HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 import matplotlib.pyplot as plt
 import numpy as np
-# Create your views here.
+import pdfkit
+import jinja2
+import base64
+
+
+def get_image_file_as_base64_data():
+    with open("images/positivity", "rb") as image_file:
+        return base64.b64encode(image_file.read())
 
 
 @csrf_exempt
 def generate_report(request: HttpRequest):
-    if request.method=="POST":
+    html_template = '<div class="header"> <h1>Domain Pulse</h1> </div>'
+
+    if request.method == "POST":
         raw_data = json.loads(request.body)
         url = f"http://localhost:{str(os.getenv('DJANGO_WAREHOUSE_PORT'))}/get_domain_dashboard/"
         overall_score = 0.8
-        val= [1-overall_score,overall_score]
+        val = [1 - overall_score, overall_score]
         val.append(sum(val))
-        colors = ['grey','blue', 'white']
-        fig = plt.figure(figsize=(8,6),dpi=100)
-        ax = fig.add_subplot(1,1,1)
+        colors = ["grey", "blue", "white"]
+        fig = plt.figure(figsize=(1.8, 1), dpi=200)
+        ax = fig.add_subplot(1, 1, 1)
         ax.pie(val, colors=colors)
-        ax.add_artist(plt.Circle((0, 0), 0.6, color='white'))
-        plt.savefig("images/generalSent.png")
+        ax.add_artist(plt.Circle((0, 0), 0.6, color="white"))
+        plt.subplots_adjust(left=-0.2, right=1.2, top=1.1, bottom=-1)
+        generalSent_file = "images/generalSent.png"
+        plt.savefig(generalSent_file)
         plt.close()
-        positive=0.6
-        neutral=0.3
-        negative=0.1
-        y = np.array([positive,neutral,negative])
-        mylabels = ["Positive","Neutral","Negative"]
-
-        plt.pie(y, labels = mylabels)
-        plt.savefig("images/PNN.png")
+        positive = 0.6
+        neutral = 0.3
+        negative = 0.1
+        y = np.array([positive, neutral, negative])
+        mylabels = ["Positive", "Neutral", "Negative"]
+        plt.figure(figsize=(2.5, 2), dpi=200)
+        plt.pie(y, labels=mylabels)
+        plt.subplots_adjust(left=-0.2, right=1, top=1, bottom=0)
+        positivity_file = "images/positivity.png"
+        plt.savefig(positivity_file)
         plt.close()
-        data = {"Joy":36,"Sadness":2,"Anger":3,"Fear":1,"Disgust":2,"Surprise":20}
+        data = {
+            "Joy": 36,
+            "Sadness": 2,
+            "Anger": 3,
+            "Fear": 1,
+            "Disgust": 2,
+            "Surprise": 20,
+        }
         emotions = list(data.keys())
         values = list(data.values())
 
-        plt.bar(emotions, values, color ='maroon',
-        width = 0.4)
+        plt.bar(emotions, values, color="maroon", width=0.4)
         plt.xlabel("Emotions")
         plt.ylabel("Number of Reviews")
 
-        plt.savefig("images/emotions.png")
+        emotion_file = "images/emotions.png"
+        plt.savefig(emotion_file)
         plt.close()
-        
-        toxic=0.05
-        nontoxic=0.95
-        y = np.array([toxic,nontoxic])
-        mylabels = ["Toxic","Non-Toxic"]
 
-        plt.pie(y, labels = mylabels)
-        plt.savefig("images/toxicity.png")
+        toxic = 0.05
+        nontoxic = 0.95
+        y = np.array([toxic, nontoxic])
+        mylabels = ["Toxic", "Non-Toxic"]
+        plt.figure(figsize=(2.8, 2), dpi=200)
+        plt.pie(y, labels=mylabels)
+        plt.subplots_adjust(left=0, right=1.1, top=1, bottom=0)
+        toxicity_file = "images/toxicity.png"
+        plt.savefig(toxicity_file)
         plt.close()
-        return JsonResponse({"status":"SUCCESS"})
+
+        output = f"<html>{html_template}<div> <h1>All Sources</h1> </div> <div> <img src=\"{str(os.getenv('PROFILES_PATH'))}{generalSent_file}\"> </div> <div> <img src=\"{str(os.getenv('PROFILES_PATH'))}{emotion_file}\"> </div><div> <img src=\"{str(os.getenv('PROFILES_PATH'))}{positivity_file}\"> </div><div> <img src=\"{str(os.getenv('PROFILES_PATH'))}{toxicity_file}\"> </div></html>"
+
+        config = pdfkit.configuration(wkhtmltopdf="/usr/bin/wkhtmltopdf")
+        output_pdf = "pdfs/pdf_generated.pdf"
+        pdfkit.from_string(
+            output,
+            output_pdf,
+            configuration=config,
+            css="css/style.css",
+            options={"enable-local-file-access": ""},
+        )
+
+        return JsonResponse({"status": "SUCCESS"})
