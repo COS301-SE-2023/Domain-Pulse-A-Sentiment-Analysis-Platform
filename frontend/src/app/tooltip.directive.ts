4150b992e129498ad4a87e38532e1f439c13c6e1
Thuthuka Khumalo <thuthuka111@gmail.com>
2023-08-27 14:18:08 +0200

    ✨ (frontend) tooltop positioning in directive

--- frontend/src/app/tooltip.directive.ts
+++ frontend/src/app/tooltip.directive.ts
@@ -12,12 +12,15 @@ export class TooltipDirective {
   }
 
   @Input('tooltip') tooltipText: string = '';
+  @Input() tooltipPosition: string = 'right'; // Default position is right
 
   @HostListener('mouseenter') onMouseEnter() {
-    const rect = this.el.nativeElement.getBoundingClientRect();
+    this.updateTooltipPosition();
     this.renderer.setStyle(this.tooltip, 'background-color', '#333');
     this.renderer.setStyle(this.tooltip, 'color', 'white');
     this.renderer.setStyle(this.tooltip, 'padding', '10px');
+    // set width
+    this.renderer.setStyle(this.tooltip, 'width', '150px');
     this.renderer.setStyle(this.tooltip, 'border-radius', '20px');
     this.renderer.setStyle(this.tooltip, 'opacity', '1');
     this.renderer.setStyle(this.tooltip, 'transform', 'translateX(0)');
@@ -25,8 +28,9 @@ export class TooltipDirective {
     this.renderer.setStyle(this.tooltip, 'position', 'absolute');
     
     // Position the tooltip to the right of the element
-    this.renderer.setStyle(this.tooltip, 'top', rect.top - 40 + 'px');
-    this.renderer.setStyle(this.tooltip, 'left', rect.right + 'px');
+    // this.renderer.setStyle(this.tooltip, 'top', rect.top - 40 + 'px');
+    // this.renderer.setStyle(this.tooltip, 'left', rect.right + 'px');
+    this.updateTooltipPosition();
 
     this.tooltip.textContent = this.tooltipText;
     this.renderer.appendChild(document.body, this.tooltip);
@@ -37,5 +41,31 @@ export class TooltipDirective {
     this.renderer.setStyle(this.tooltip, 'transform', 'translateX(-20px)');
     this.tooltip.textContent = '';
     this.renderer.removeChild(this.el.nativeElement, this.tooltip);
+  }
+
+  private updateTooltipPosition() {
+    const rect = this.el.nativeElement.getBoundingClientRect();
+    console.log("left", rect.left);
+    console.log("top", rect.top);
+    console.log("width", rect.width);
+    console.log("height", rect.height);
+    switch (this.tooltipPosition) {
+      case 'top':
+        this.renderer.setStyle(this.tooltip, 'top', rect.top - 40 + 'px');
+        this.renderer.setStyle(this.tooltip, 'left', rect.left + rect.width / 2 + 'px');
+        break;
+      case 'right':
+        this.renderer.setStyle(this.tooltip, 'top', rect.top - 40 + 'px');
+        this.renderer.setStyle(this.tooltip, 'left', rect.right + 'px');
+        break;
+      case 'bottom':
+        this.renderer.setStyle(this.tooltip, 'top', rect.bottom + 'px');
+        this.renderer.setStyle(this.tooltip, 'left', rect.left + rect.width / 2 + 'px');
+        break;
+      case 'left':
+        this.renderer.setStyle(this.tooltip, 'top', rect.top - 40 + 'px');
+        this.renderer.setStyle(this.tooltip, 'left', rect.left - rect.width - 150  + 'px');
+        break;
+    }
   }
 }
