3bca3196f5d0ad0689c3a14d82d412e7aaf3c134
chrislom12 <christiaanlombard12@gmail.com>
2023-08-31 23:05:25 +0200

    🚧 (UI) Implementing apex charts

--- frontend/src/app/graph-selector/graph-selector.component.ts
+++ frontend/src/app/graph-selector/graph-selector.component.ts
@@ -4,6 +4,31 @@ import { Select, Store } from '@ngxs/store';
 import { AppState } from '../app.state';
 import { Observable } from 'rxjs';
 import { ChooseStatistic } from '../app.actions';
+import {
+  ChartComponent,
+  ApexAxisChartSeries,
+  ApexChart,
+  ApexXAxis,
+  ApexTitleSubtitle,
+  ApexDataLabels,
+  ApexYAxis,
+  ApexLegend,
+  ApexFill
+} from "ng-apexcharts";
+
+export type ChartOptions = {
+  series: ApexAxisChartSeries;
+  chart: ApexChart;
+  xaxis: ApexXAxis;
+  title: ApexTitleSubtitle;
+  dataLabels: ApexDataLabels;
+  colors: string[];
+  legend: ApexLegend;
+  fill: ApexFill;
+  yaxis: ApexYAxis;
+
+
+};
 
 @Component({
   selector: 'graph-selector',
@@ -11,8 +36,17 @@ import { ChooseStatistic } from '../app.actions';
   styleUrls: ['./graph-selector.component.sass'],
 })
 export class GraphSelectorComponent implements OnInit {
+
+  
   @ViewChild('myChart') myChart!: ElementRef;
   @ViewChild('chartContainer') chartContainer!: ElementRef;
+
+  @ViewChild("chart")
+  chart!: ChartComponent;
+  chartOptions!: Partial<ChartOptions>;
+  chartOptionsArray: any[] | Partial<ChartOptions>[] = [];
+
+  
 
   @Select(AppState.sourceOverallSentimentScores)
   sourceOverallSentiment!: Observable<any | null>;
@@ -20,9 +54,9 @@ export class GraphSelectorComponent implements OnInit {
   currentGraphIndex: number = 0;
   @Select(AppState.sourceIsLoading) sourceIsLoading$!: Observable<boolean>;
 
-  chart: Chart | undefined;
-  gradient: CanvasGradient | undefined;
-  graphs = [
+  //chart: Chart | undefined;
+  //gradient: CanvasGradient | undefined;
+ /*  graphs = [
     {
       type: 'doughnut',
       fontColor: 'white',
@@ -187,60 +221,20 @@ export class GraphSelectorComponent implements OnInit {
         },
       },
     },
-    /* {
-      type: 'bar', 
-      data: {
-        labels: ['Facebook', 'Instagram', 'Reddit'], 
-        datasets: [
-          {
-            label: 'Nr. of Sentiments', 
-            data: [23000, 17530, 3450], 
-            backgroundColor: [
-              'rgba(3, 127, 255, 0.8)',
-              'rgba(145, 44, 246, 0.8)',
-              'rgba(23, 35, 76, 0.8)',
-            ], 
-            borderColor: [
-              'rgba(3, 127, 255, 1)',
-              'rgba(145, 44, 246, 1)',
-              'rgba(23, 35, 76, 1)',
-            ],
-            borderWidth: 1,
-          },
-        ],
-      },
-      options: {
-        title: {
-          display: true,
-          text: 'Number of Sentiments Analysed per Source',
-          align: 'left',
-        },
-        responsive: true,
-        drawChartArea: false,
-        maintainAspectRatio: false,
-        interaction: {
-          mode: 'nearest', 
-          intersect: false, 
-        },
-        legend: {
-          display: false,
-        },
-      },
-    } */
-
-    /* Define your different graphs here */
-    // { labels: ['Label 1', 'Label 2', 'Label 3'], data: [10, 20, 30], ... },
-    // { labels: ['Label A', 'Label B', 'Label C'], data: [50, 40, 30], ... },
-    // ...
+    
   ];
+ */
 
   showPopup(index: number) {
     console.log('Clicked on section:', index);
   }
-  constructor(private store: Store) {}
+
+  constructor(private store: Store) {
+  }
+
 
   ngOnInit(): void {
-    this.sourceOverallSentiment.subscribe((data) => {
+    /* this.sourceOverallSentiment.subscribe((data) => {
       console.log(data);
       if (data) {
         if (data.aggregated_metrics.general.category != 'No data') {
@@ -253,18 +247,269 @@ export class GraphSelectorComponent implements OnInit {
           }, 300);
         }
       }
-    });
+    }); */
+    
 
-    this.statisticIndex.subscribe((statIndex) => {
+    /* this.statisticIndex.subscribe((statIndex) => {
       if (statIndex !== this.currentGraphIndex && statIndex !== undefined) {
         this.currentGraphIndex = statIndex;
         this.renderGraph();
       }
-    });
+    }); */
+
+      this.chartOptionsArray = [
+      {
+        series: [
+          {
+            name: "My-series",
+            data: [10, 41, 35, 51, 49, 62, 69, 91, 148]
+          }
+        ],
+        chart: {
+          height: '100%',
+          type: 'bar'
+        },
+        title: {
+          text: 'Chart 1'
+        },
+        xaxis: {
+          categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep']
+        }
+      },
+      {
+        series: [
+          {
+            name: "My-series",
+            data: [1, 1, 1, 200, 200, 1, 3, 3, 40]
+          }
+        ],
+        chart: {
+          height: '100%',
+          type: 'line'
+        },
+        title: {
+          text: 'Chart 2'
+        },
+        xaxis: {
+          categories: ['A', 'B', 'C', 'D', 'E']
+        }
+      },
+      {
+        series: [
+          {
+            name: "My-series",
+            data: [
+              ['2023-01-01', 10],
+              ['2023-02-01', 41],
+              ['2023-03-01', 35],
+              // ... other data points
+            ]
+          }
+        ],
+        chart: {
+          height: '100%',
+          type: 'line'
+        },
+        title: {
+          text: 'Time Series Chart'
+        },
+        xaxis: {
+          type: 'datetime',
+          categories: [
+            '2023-01-01',
+            '2023-02-01',
+            '2023-03-01',
+            // ... other date strings
+          ]
+        }
+      },
+      {
+        series: [
+          {
+            name: "My-series",
+            data: [
+              ['2023-08-20T00:00', 10],
+              ['2023-08-21T03:00', 41],
+              ['2023-08-22T06:00', 35],
+              ['2023-08-23T00:00', 10],
+              ['2023-08-23T03:00', 41],
+              ['2023-08-23T06:00', 35],
+              ['2023-08-24T00:00', 10],
+              ['2023-08-25T03:00', 41],
+              ['2023-08-25T06:00', 35],
+              ['2023-08-26T00:00', 10],
+              ['2023-08-26T03:00', 41],
+              ['2023-08-26T06:00', 35],
+              ['2023-08-27T00:00', 10],
+              ['2023-08-31T03:00', 41],
+              ['2023-08-31T06:00', 35],
+              // ... other data points
+            ]
+          }
+        ],
+        chart: {
+          height: '100%',
+          type: 'area'
+        },
+        title: {
+          text: 'Specific Day and Time Series Chart'
+        },
+        xaxis: {
+          type: 'datetime',
+          categories: [
+            '2023-08-20T00:00',
+            '2023-08-21T03:00',
+            '2023-08-22T06:00',
+            '2023-08-23T00:00',
+            '2023-08-23T03:00',
+            '2023-08-23T06:00',
+            '2023-08-24T00:00',
+            '2023-08-25T03:00',
+            '2023-08-25T06:00',
+            '2023-08-26T00:00',
+            '2023-08-26T03:00',
+            '2023-08-26T06:00',
+            '2023-08-27T00:00',
+            '2023-08-31T03:00',
+            '2023-08-31T06:00',
+            // ... other date and time strings
+          ]
+        }
+      },
+      {
+        series: [
+          {
+            name: "Positive",
+            data: [['2023-08-20T00:00', 10],
+            ['2023-08-21T03:00', 10],
+            ['2023-08-22T06:00', 40],
+            ['2023-08-23T00:00', 0],
+            ['2023-08-23T03:00', 10],]
+          },
+          {
+            name: "Negative",
+            data: [['2023-08-20T00:00', 10],
+            ['2023-08-21T03:00', 60],
+            ['2023-08-22T06:00', 40],
+            ['2023-08-23T00:00', 50],
+            ['2023-08-23T03:00', 40],
+  ]
+          },
+          {
+            name: "Neutral",
+            data: [['2023-08-20T00:00', 10],
+            ['2023-08-21T03:00', 30],
+            ['2023-08-22T06:00', 20],
+            ['2023-08-23T00:00', 50],
+            ['2023-08-23T03:00', 40],]
+          }
+        ],
+        chart: {
+          height: '100%',
+          type: 'area'
+        },
+        title: {
+          text: 'Specific Day and Time Series Chart'
+        },
+        xaxis: {
+          type: 'datetime',
+          categories: [
+            '2023-08-21T03:00',
+'2023-08-22T06:00',
+'2023-08-23T00:00',
+'2023-08-23T03:00',
+            // ... other date and time strings
+          ]
+        }
+      },
+      {
+        series: [
+          {
+            name: "Positive",
+            data: [['2023-08-20T00:00', 10],
+            ['2023-08-21T03:00', 10],
+            ['2023-08-22T06:00', 40],
+            ['2023-08-23T00:00', 0],
+            ['2023-08-23T03:00', 10],]
+          },
+          {
+            name: "Negative",
+            data: [['2023-08-20T00:00', 10],
+            ['2023-08-21T03:00', 60],
+            ['2023-08-22T06:00', 40],
+            ['2023-08-23T00:00', 50],
+            ['2023-08-23T03:00', 40],
+  ]
+          },
+          {
+            name: "Neutral",
+            data: [['2023-08-20T00:00', 10],
+            ['2023-08-21T03:00', 30],
+            ['2023-08-22T06:00', 20],
+            ['2023-08-23T00:00', 50],
+            ['2023-08-23T03:00', 40],]
+          }
+        ],
+        chart: {
+          type: "area",
+          height: 350,
+          stacked: true,
+          events: {
+            selection: function(chart, e) {
+              console.log(new Date(e.xaxis.min));
+            }
+          }
+        },
+        colors: ["#008FFB", "#00E396", "#CED4DC"],
+        dataLabels: {
+          enabled: false
+        },
+        fill: {
+          type: "gradient",
+          gradient: {
+            opacityFrom: 0.6,
+            opacityTo: 0.8
+          }
+        },
+        legend: {
+          position: "top",
+          horizontalAlign: "left"
+        },
+        xaxis: {
+          type: "datetime"
+        }
+      }
+    
+      // Add more chartOptions as needed
+    ];
+
+    this.chartOptions = this.chartOptionsArray[0];
+
+    
   }
 
+  public generateDayWiseTimeSeries = function(baseval: number, count: number, yrange: { min: any; max: any; }) {
+    var i = 0;
+    var series = [];
+    while (i < count) {
+      var x = baseval;
+      var y =
+        Math.floor(Math.random() * (yrange.max - yrange.min + 1)) + yrange.min;
+
+      series.push([x, y]);
+      baseval += 86400000;
+      i++;
+    }
+    return series;
+  };
+
   ngAfterViewInit() {
-    this.renderGraph();
+
+    console.log('after view init')
+    // Access and manipulate the DOM element in ngAfterViewInit
+    setTimeout(() => {
+      this.renderGraph();
+    });
   }
 
   assignGraphData(mockData: any, graphArray: any[]): any[] {
@@ -304,7 +549,7 @@ export class GraphSelectorComponent implements OnInit {
 
   updatedGraphArray?: any[];
 
-  switchToPreviousGraph() {
+  /* switchToPreviousGraph() {
     if (!this.updatedGraphArray) return;
 
     console.log('previous graph');
@@ -329,17 +574,63 @@ export class GraphSelectorComponent implements OnInit {
       this.renderGraph();
     }
     this.store.dispatch(new ChooseStatistic(this.currentGraphIndex));
+  } */
+
+  switchToPreviousGraph() {
+    if (!this.chartOptionsArray || !this.chartOptionsArray.length) {
+      return;
+    }
+  
+    this.currentGraphIndex = (this.currentGraphIndex - 1 + this.chartOptionsArray.length) % this.chartOptionsArray.length;
+  
+    this.store.dispatch(new ChooseStatistic(this.currentGraphIndex));
+  
+    this.renderGraph();
+  }
+  
+  switchToNextGraph() {
+    if (!this.chartOptionsArray || !this.chartOptionsArray.length) {
+      return;
+    }
+  
+    this.currentGraphIndex = (this.currentGraphIndex + 1) % this.chartOptionsArray.length;
+  
+    this.store.dispatch(new ChooseStatistic(this.currentGraphIndex));
+  
+    this.renderGraph();
   }
 
   renderGraph() {
+    console.log('rendering graph at index: ' + this.currentGraphIndex);
+
+    /* if (!this.updatedGraphArray || !this.chartOptionsArray[this.currentGraphIndex]) {
+      return;
+    } */
+
+    const container = this.chartContainer.nativeElement;
+    const containerHeight = container.offsetHeight;
+
+    // Update chart height based on container's height
+    if(this.chartOptionsArray[this.currentGraphIndex].chart?.height){
+      this.chartOptionsArray[this.currentGraphIndex].chart.height = containerHeight;
+    }
+
+  
+    this.chartOptions = this.chartOptionsArray[this.currentGraphIndex];
+  
+    // Render the chart with the updated options
+    // Replace 'apexchart' with the appropriate chart component and attributes
+    //this.chart.updateOptions(this.chartOptions);
+
+
     if (!this.myChart || !this.chartContainer) {
       return;
     }
 
-    if (this.chart) {
+    /* if (this.chart) {
       this.chart.destroy();
     }
-
+ */
     const element = document.querySelector('body'); // Replace with your actual element selector
     if (element) {
       const styles = window.getComputedStyle(element);
@@ -347,7 +638,7 @@ export class GraphSelectorComponent implements OnInit {
       Chart.defaults.global.defaultFontColor = backgroundColor;
     }
 
-    const ctx = this.myChart.nativeElement.getContext('2d');
+    /* const ctx = this.myChart.nativeElement.getContext('2d');
 
     const container = this.chartContainer.nativeElement;
     const containerWidth = container.offsetWidth;
@@ -371,6 +662,7 @@ export class GraphSelectorComponent implements OnInit {
       this.myChart.nativeElement.style.transform = `translateY(${offset}px)`;
     } else {
       this.myChart.nativeElement.style.transform = `translateY(0px)`;
-    }
-  }
+    }*/
+  } 
+  
 }
