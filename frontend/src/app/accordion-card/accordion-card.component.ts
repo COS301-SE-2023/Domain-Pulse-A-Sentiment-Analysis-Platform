e7d46cd9119352b650f8d74f4c4705eebae60a83
Caden Dobson <106334873+cadendobson@users.noreply.github.com>
2023-09-04 16:20:01 +0200

    🚧 (frontend) added animations for accordion

---
+++ frontend/src/app/accordion-card/accordion-card.component.ts
@@ -0,0 +1,19 @@
+import { Component ,Input } from '@angular/core';
+import {trigger,style,state,transition,animate} from '@angular/animations';
+
+@Component({
+  selector: 'app-accordion-card',
+  templateUrl: './accordion-card.component.html',
+  styleUrls: ['./accordion-card.component.sass'],
+  animations: [
+    trigger('expandCollapse', [
+      state('collapsed', style({height: '0px', opacity: 0})),
+      state('expanded', style({height: 'auto', opacity: 1})),
+      transition('collapsed => expanded', animate('300ms ease-in')),
+      transition('expanded => collapsed', animate('300ms ease-out'))
+    ])
+  ]
+})
+export class AccordionCardComponent {
+
+}
