6de240882ffed2903544be4530e2c3e4f53c669d
Luke Lawson <u21433811@tuks.co.za>
2023-09-06 10:53:10 +0200

    ✅ (backend) Added tests for toxicity time series

--- backend/engine/aggregator/tests.py
+++ backend/engine/aggregator/tests.py
@@ -312,6 +312,208 @@ class DataAggregationTests(TestCase):
             },
         }
 
+    def test_toxicity_aggregation_timeseries(self):
+        test_metrics = {
+            "metrics": [
+                {
+                    "data": "Lived up to the expectations. Grab and run... pleasant and fast service. Definitely a must visit again",
+                    "general": {"category": "VERY_POSITIVE", "score": 0.9994},
+                    "emotions": {"surprise": 0.015, "neutral": 0.0817, "joy": 0.9033},
+                    "toxicity": {"level_of_toxic": "Toxic", "score": 0.0005},
+                    "ratios": {"positive": 0.353, "neutral": 0.647, "negative": 0.0},
+                    "timestamp": 1690542558,
+                },
+                {
+                    "data": "Starbucks is good because there is always good coffee, there is Wi-Fi Internet and your name will be loudly called here once. It is right if it is simple and wrong if it is unusual. All of the above is in this Starbucks, which is conveniently located at a significant intersection in Rosebank.",
+                    "general": {"category": "VERY_NEGATIVE", "score": 0.0296},
+                    "emotions": {"surprise": 0.514, "joy": 0.0918, "neutral": 0.3942},
+                    "toxicity": {"level_of_toxic": "Toxic", "score": 0.0004},
+                    "ratios": {"positive": 0.226, "neutral": 0.682, "negative": 0.092},
+                    "timestamp": 1690442558,
+                },
+                {
+                    "data": "I just got looked at when I was at the wrong side of the line (not clear where the right side is) Could not spell my name correctly even though I spelt it out 3 times.",
+                    "general": {"category": "VERY_NEGATIVE", "score": 0.0012},
+                    "emotions": {
+                        "surprise": 0.1251,
+                        "neutral": 0.6451,
+                        "sadness": 0.2298,
+                    },
+                    "toxicity": {"level_of_toxic": "Toxic", "score": 0.0004},
+                    "ratios": {"positive": 0.097, "neutral": 0.787, "negative": 0.116},
+                    "timestamp": 1690552558,
+                },
+                {
+                    "data": "Everything is amazing about Starbucks The service is just so good The drinks are the absolute best Keep coming back ⭐️⭐️⭐️⭐️⭐️👍",
+                    "general": {"category": "VERY_POSITIVE", "score": 0.9997},
+                    "emotions": {"surprise": 0.0683, "joy": 0.8565, "neutral": 0.0752},
+                    "toxicity": {"level_of_toxic": "Toxic", "score": 0.0005},
+                    "ratios": {"positive": 0.391, "neutral": 0.609, "negative": 0.0},
+                    "timestamp": 1690432558,
+                },
+                {
+                    "data": "Just bad choice of blend. Poorly made. Really bad texture of frothed milk. Very hyped. Seattle Coffee Co. still the best.",
+                    "general": {"category": "VERY_NEGATIVE", "score": 0.0005},
+                    "emotions": {
+                        "sadness": 0.7847,
+                        "disgust": 0.0617,
+                        "neutral": 0.1536,
+                    },
+                    "toxicity": {"level_of_toxic": "Toxic", "score": 0.0009},
+                    "ratios": {"positive": 0.133, "neutral": 0.635, "negative": 0.232},
+                    "timestamp": 1690568558,
+                },
+                {
+                    "data": "If I could live here, I would. i love Starbucks on Rosebank that much. The best blueberry muffins I have ever tasted. Great coffee as well :-)",
+                    "general": {"category": "VERY_POSITIVE", "score": 0.9999},
+                    "emotions": {"sadness": 0.0421, "joy": 0.9376, "surprise": 0.0203},
+                    "toxicity": {"level_of_toxic": "Toxic", "score": 0.0005},
+                    "ratios": {"positive": 0.388, "neutral": 0.612, "negative": 0.0},
+                    "timestamp": 1690568558,
+                },
+                {
+                    "data": "Coffee is always Good , but unfortunately the food is not up to Starbucks quality Like overseas , the food is premade and delivered , so if you eat a toasted cheese it just get heated up in the microwave",
+                    "general": {"category": "VERY_POSITIVE", "score": 0.9571},
+                    "emotions": {
+                        "sadness": 0.8615,
+                        "surprise": 0.1088,
+                        "neutral": 0.0298,
+                    },
+                    "toxicity": {"level_of_toxic": "Toxic", "score": 0.0007},
+                    "ratios": {"positive": 0.209, "neutral": 0.698, "negative": 0.093},
+                    "timestamp": 1690568558,
+                },
+                {
+                    "data": "We have just moved into Melrose and this Starbucks is our local coffee bar. You can get tickets (if resident) so that everytime you buy a coffee you get the fifth one free. Great staff that get to know your name and what you are drinking.",
+                    "general": {"category": "VERY_POSITIVE", "score": 0.9714},
+                    "emotions": {"surprise": 0.1751, "neutral": 0.318, "joy": 0.5069},
+                    "toxicity": {"level_of_toxic": "Toxic", "score": 0.0004},
+                    "ratios": {"positive": 0.215, "neutral": 0.785, "negative": 0.0},
+                    "timestamp": 1690568558,
+                },
+            ]
+        }
+
+        result = aggregation.aggregate_sentiment_data(test_metrics["metrics"])
+
+        assert result["timeseries"] == {
+            "overall": [
+                ["2023-07-27T06:35:58", 0.9997],
+                ["2023-07-27T09:22:38", 0.7087],
+                ["2023-07-28T13:09:18", 0.7959],
+                ["2023-07-28T15:55:58", 0.5575],
+                ["2023-07-28T20:22:38", 0.3904],
+                ["2023-07-28T20:22:38", 0.5733],
+                ["2023-07-28T20:22:38", 0.6884],
+                ["2023-07-28T20:22:38", 0.7733],
+            ],
+            "emotions": {
+                "anger": [
+                    ["2023-07-27T06:35:58", 0],
+                    ["2023-07-27T09:22:38", 0.0],
+                    ["2023-07-28T13:09:18", 0.0],
+                    ["2023-07-28T15:55:58", 0.0],
+                    ["2023-07-28T20:22:38", 0.0],
+                    ["2023-07-28T20:22:38", 0.0],
+                    ["2023-07-28T20:22:38", 0.0],
+                    ["2023-07-28T20:22:38", 0.0],
+                ],
+                "sadness": [
+                    ["2023-07-27T06:35:58", 0],
+                    ["2023-07-27T09:22:38", 0.0],
+                    ["2023-07-28T13:09:18", 0.0],
+                    ["2023-07-28T15:55:58", 0.1149],
+                    ["2023-07-28T20:22:38", 0.4498],
+                    ["2023-07-28T20:22:38", 0.246],
+                    ["2023-07-28T20:22:38", 0.5537],
+                    ["2023-07-28T20:22:38", 0.443],
+                ],
+                "joy": [
+                    ["2023-07-27T06:35:58", 0.8565],
+                    ["2023-07-27T09:22:38", 0.4742],
+                    ["2023-07-28T13:09:18", 0.6887],
+                    ["2023-07-28T15:55:58", 0.551],
+                    ["2023-07-28T20:22:38", 0.4408],
+                    ["2023-07-28T20:22:38", 0.6892],
+                    ["2023-07-28T20:22:38", 0.5514],
+                    ["2023-07-28T20:22:38", 0.5292],
+                ],
+                "digust": [
+                    ["2023-07-27T06:35:58", 0],
+                    ["2023-07-27T09:22:38", 0.0],
+                    ["2023-07-28T13:09:18", 0.0],
+                    ["2023-07-28T15:55:58", 0.0],
+                    ["2023-07-28T20:22:38", 0.0],
+                    ["2023-07-28T20:22:38", 0.0],
+                    ["2023-07-28T20:22:38", 0.0],
+                    ["2023-07-28T20:22:38", 0.0],
+                ],
+                "fear": [
+                    ["2023-07-27T06:35:58", 0],
+                    ["2023-07-27T09:22:38", 0.0],
+                    ["2023-07-28T13:09:18", 0.0],
+                    ["2023-07-28T15:55:58", 0.0],
+                    ["2023-07-28T20:22:38", 0.0],
+                    ["2023-07-28T20:22:38", 0.0],
+                    ["2023-07-28T20:22:38", 0.0],
+                    ["2023-07-28T20:22:38", 0.0],
+                ],
+                "surprise": [
+                    ["2023-07-27T06:35:58", 0.0683],
+                    ["2023-07-27T09:22:38", 0.2912],
+                    ["2023-07-28T13:09:18", 0.1531],
+                    ["2023-07-28T15:55:58", 0.1391],
+                    ["2023-07-28T20:22:38", 0.1113],
+                    ["2023-07-28T20:22:38", 0.0658],
+                    ["2023-07-28T20:22:38", 0.0873],
+                    ["2023-07-28T20:22:38", 0.1312],
+                ],
+            },
+            "toxicity": [{"x": "2023-07-27", "y": 2}, {"x": "2023-07-28", "y": 6}],
+            "ratios": {
+                "pos": [
+                    ["2023-07-27T06:35:58", 0.391],
+                    ["2023-07-27T09:22:38", 0.3415],
+                    ["2023-07-28T13:09:18", 0.3449],
+                    ["2023-07-28T15:55:58", 0.2705],
+                    ["2023-07-28T20:22:38", 0.2292],
+                    ["2023-07-28T20:22:38", 0.2768],
+                    ["2023-07-28T20:22:38", 0.2565],
+                    ["2023-07-28T20:22:38", 0.244],
+                ],
+                "neu": [
+                    ["2023-07-27T06:35:58", 0.609],
+                    ["2023-07-27T09:22:38", 0.6309],
+                    ["2023-07-28T13:09:18", 0.6357],
+                    ["2023-07-28T15:55:58", 0.6811],
+                    ["2023-07-28T20:22:38", 0.6673],
+                    ["2023-07-28T20:22:38", 0.6507],
+                    ["2023-07-28T20:22:38", 0.6649],
+                    ["2023-07-28T20:22:38", 0.7009],
+                ],
+                "neg": [
+                    ["2023-07-27T06:35:58", 0.0],
+                    ["2023-07-27T09:22:38", 0.0276],
+                    ["2023-07-28T13:09:18", 0.0193],
+                    ["2023-07-28T15:55:58", 0.0483],
+                    ["2023-07-28T20:22:38", 0.1034],
+                    ["2023-07-28T20:22:38", 0.0724],
+                    ["2023-07-28T20:22:38", 0.0786],
+                    ["2023-07-28T20:22:38", 0.055],
+                ],
+            },
+            "num_records": [
+                ["2023-07-27T06:35:58", 1],
+                ["2023-07-27T09:22:38", 2],
+                ["2023-07-28T13:09:18", 3],
+                ["2023-07-28T15:55:58", 4],
+                ["2023-07-28T20:22:38", 5],
+                ["2023-07-28T20:22:38", 6],
+                ["2023-07-28T20:22:38", 7],
+                ["2023-07-28T20:22:38", 8],
+            ],
+        }
+
     # ----------------------------------------------------------------
 
     # ---------------------- INTEGRATION TESTS -----------------------
