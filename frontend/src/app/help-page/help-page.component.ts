0a1c32ec8f99343c74e54c2dfe20aba16d8a4fd8
Caden Dobson <106334873+cadendobson@users.noreply.github.com>
2023-09-14 22:33:44 +0200

    ✨(frontend) added variables for help page search

--- frontend/src/app/help-page/help-page.component.ts
+++ frontend/src/app/help-page/help-page.component.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 //import {AccordionModule} from "ngx-accordion";
-
-
 @Component({
   selector: 'app-help-page',
   templateUrl: './help-page.component.html',
   styleUrls: ['./help-page.component.sass']
 })
-export class HelpPageComponent {
+export class HelpPageComponent implements AfterViewInit {
   collapsing = true;
+  searchTerm = "";
+  accordionItems: NodeListOf<Element> | undefined;
+
 }
