ecb3ad866bd4e5d2a6d0b007d99e03ca4f44144e
chrislom12 <christiaanlombard12@gmail.com>
2023-09-16 13:33:58 +0200

    🗑️ (frontend) Clean up graph selector

--- frontend/src/app/graph-selector/graph-selector.component.ts
+++ frontend/src/app/graph-selector/graph-selector.component.ts
@@ -698,208 +698,14 @@ export class GraphSelectorComponent implements OnInit {
       },
     ],
 
-    // Add more chartOptions as needed
   ];
-  //chart: Chart | undefined;
-  //gradient: CanvasGradient | undefined;
-  /*  graphs = [
-    {
-      type: 'doughnut',
-      fontColor: 'white',
-      data: {
-        labels: ['Overall Score'],
-        datasets: [
-          {
-            data: [],
-            backgroundColor: [
-              'rgba(3, 127, 255, 1)',
-              'rgba(170, 170, 170, 0.71)',
-              'rgba(0, 0, 0, 0)',
-            ],
-            borderWidth: 0,
-          },
-        ],
-      },
-      options: {
-        onClick: (event: any, elements: string | any[]) => {
-          if (elements && elements.length > 0) {
-            const clickedIndex = elements[0]._index;
-            this.showPopup(clickedIndex);
-          }
-        },
-        responsive: true,
-        maintainAspectRatio: false,
-        cutout: '50%',
-        rotation: 1 * Math.PI,
-        tooltips: {
-          enabled: false,
-        },
-        plugins: {
-          legend: {
-            display: false,
-          },
-          labels: {
-            fontColor: 'white',
-          },
-        },
-      },
-    },
-    {
-      type: 'pie',
-      data: {
-        labels: ['Positive', 'Negative', 'Neutral'],
-        datasets: [
-          {
-            data: [],
-            backgroundColor: [
-              'rgba(75, 192, 192, 0.8)',
-              'rgba(255, 99, 132, 0.8)',
-              'rgba(54, 162, 235, 0.8)',
-            ],
-            borderColor: [
-              'rgba(75, 192, 192, 1)',
-              'rgba(255, 99, 132, 1)',
-              'rgba(54, 162, 235, 1)',
-            ],
-            borderWidth: 1,
-          },
-        ],
-      },
-      options: {
-        onClick: (event: any, elements: string | any[]) => {
-          if (elements && elements.length > 0) {
-            const clickedIndex = elements[0]._index;
-            this.showPopup(clickedIndex);
-          }
-        },
-      },
-    },
-    {
-      type: 'bar',
-      data: {
-        labels: [
-          'anger',
-          'disgust',
-          'fear',
-          'joy',
-          'neutral',
-          'sadness',
-          'surprise',
-        ],
-        datasets: [
-          {
-            label: 'Rating per Emotion',
-            data: [],
-            backgroundColor: [
-              'rgba(3, 127, 255, 0.8)',
-              'rgba(145, 44, 246, 0.8)',
-              'rgba(23, 35, 76, 0.8)',
-              'rgba(255, 99, 132, 0.8)',
-              'rgba(54, 162, 235, 0.8)',
-              'rgba(255, 206, 86, 0.8)',
 
-              'rgba(75, 192, 192, 0.8)',
-            ],
-
-            borderColor: [
-              'rgba(3, 127, 255, 1)',
-              'rgba(145, 44, 246, 1)',
-              'rgba(23, 35, 76, 1)',
-              'rgba(255, 99, 132, 1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-              'rgba(75, 192, 192, 1)',
-            ],
-
-            borderWidth: 1,
-          },
-        ],
-      },
-      options: {
-        title: {
-          display: true,
-          text: 'Number of Sentiments Analysed per Source',
-          align: 'left',
-        },
-        responsive: true,
-        drawChartArea: false,
-        maintainAspectRatio: false,
-        interaction: {
-          mode: 'nearest',
-          intersect: false,
-        },
-        legend: {
-          display: false,
-        },
-        onClick: (event: any, elements: string | any[]) => {
-          if (elements && elements.length > 0) {
-            const clickedIndex = elements[0]._index;
-            this.showPopup(clickedIndex);
-          }
-        },
-      },
-    },
-    {
-      type: 'pie',
-      data: {
-        labels: ['Toxic', 'Non-Toxic'],
-        datasets: [
-          {
-            data: [],
-            backgroundColor: [
-              'rgba(3, 127, 255, 0.8)',
-              'rgba(145, 44, 246, 0.8)',
-            ],
-            borderColor: [
-              'rgba(3, 127, 255, 1)',
-              'rgba(145, 44, 246, 1)', // Border color for slice 3
-            ],
-            borderWidth: 1,
-          },
-        ],
-      },
-      options: {
-        onClick: (event: any, elements: string | any[]) => {
-          if (elements && elements.length > 0) {
-            const clickedIndex = elements[0]._index;
-            this.showPopup(clickedIndex);
-          }
-        },
-      },
-    },
-    
-  ];
- */
 
   showPopup(index: number) {
     console.log('Clicked on section:', index);
   }
 
   constructor(private store: Store) {}
-
-  getColor(value: number): string {
-    if (value < 10) {
-      return 'rgba(255, 56, 56, 0.8)'; // Dark Red
-    } else if (value < 20) {
-      return 'rgba(219, 85, 85, 0.8)'; // Red
-    } else if (value < 30) {
-      return 'rgba(255, 136, 100, 0.8)'; // Red-Orange
-    } else if (value < 40) {
-      return 'rgba(255, 204, 108, 0.8)'; // Orange
-    } else if (value < 50) {
-      return 'rgba(255, 232, 101, 0.8)'; // Gold
-    } else if (value < 60) {
-      return 'rgba(214, 255, 101, 0.8)'; // Light Blue
-    } else if (value < 70) {
-      return 'rgba(186, 255, 122, 0.8)'; // Green
-    } else if (value < 80) {
-      return 'rgba(141, 255, 113, 0.8)'; // Lime Green
-    } else if (value < 90) {
-      return 'rgba(60, 253, 85, 0.8)'; // Cyan
-    } else {
-      return 'rgba(29, 207, 44, 0.8)'; // Blue
-    }
-  }
   
   
 
@@ -937,63 +743,17 @@ export class GraphSelectorComponent implements OnInit {
   }
 
   private updateDots(selectedIndex: number) {
-    // Replace this with logic to get the dots array from your state or data source
-    // For now, we'll use a dummy array to represent the dots
-    const dotsArray = [2, 2, 3, 2, 1]; // Replace with your data source or state
 
-    // Ensure the selectedIndex is within bounds
+    const dotsArray = [2, 2, 3, 2, 1]; 
+
     if (selectedIndex >= 0 && selectedIndex < dotsArray.length) {
       this.dots = new Array(dotsArray[selectedIndex]).fill(0);
     } else {
-      this.dots = []; // Handle invalid selectedIndex
+      this.dots = []; 
     }
   }
 
-  public generateData(count: number, yrange: { max: number; min: number }) {
-    var i = 0;
-    var series = [];
-    while (i < count) {
-      var x = 'w' + (i + 1).toString();
-      var y =
-        Math.floor(Math.random() * (yrange.max - yrange.min + 1)) + yrange.min;
-
-      series.push({
-        x: x,
-        y: y,
-      });
-      i++;
-    }
-
-    console.log(series);
-    return series;
-  }
-
-  public generateDayWiseTimeSeries = function (
-    baseval: number,
-    count: number,
-    yrange: { min: any; max: any }
-  ) {
-    var i = 0;
-    var series = [];
-    while (i < count) {
-      var x = baseval;
-      var y =
-        Math.floor(Math.random() * (yrange.max - yrange.min + 1)) + yrange.min;
-
-      series.push([x, y]);
-      baseval += 86400000;
-      i++;
-    }
-    return series;
-  };
-
-  ngAfterViewInit() {
-    /*    console.log('after view init');
-    // Access and manipulate the DOM element in ngAfterViewInit
-    setTimeout(() => {
-      this.renderGraph();
-    }); */
-  }
+  
 
   assignGraphData(
     aggregated_metrics: any,
@@ -1003,11 +763,9 @@ export class GraphSelectorComponent implements OnInit {
     const aggregatedMetrics = aggregated_metrics;
     const timeseriesData = timeseries;
 
-    // Update the first graph (doughnut)
     const score = aggregatedMetrics.general.score * 100;
     graphArray[0][0].series = [score];
 
-    // Update the second graph (pie)
     graphArray[1][0].series = [
       Math.floor(aggregatedMetrics.ratios.positive * 100),
       Math.floor(aggregatedMetrics.ratios.negative * 100),
@@ -1033,13 +791,9 @@ export class GraphSelectorComponent implements OnInit {
       Math.floor(aggregatedMetrics.emotions.sadness * 100),
     ];
 
-    // Update the fourth graph (pie)
     graphArray[3][0].series = [
       Math.floor(aggregatedMetrics.toxicity.score * 100),
     ];
-
-    console.log('time series');
-    console.log(timeseriesData);
 
     graphArray[0][1].series[0].data = timeseriesData.overall;
 
@@ -1047,8 +801,7 @@ export class GraphSelectorComponent implements OnInit {
     graphArray[1][1].series[1].data = timeseriesData.ratios.neg;
     graphArray[1][1].series[2].data = timeseriesData.ratios.neu;
 
-    console.log('emotions');
-    console.log(timeseriesData.emotions.anger);
+
     graphArray[2][1].series[0].data = timeseriesData.emotions.anger;
     graphArray[2][1].series[1].data = timeseriesData.emotions.disgust;
     graphArray[2][1].series[2].data = timeseriesData.emotions.fear;
@@ -1067,32 +820,7 @@ export class GraphSelectorComponent implements OnInit {
 
   updatedGraphArray?: any[];
 
-  /* switchToPreviousGraph() {
-    if (!this.updatedGraphArray) return;
-
-    console.log('previous graph');
-    if (this.currentGraphIndex > 0) {
-      this.currentGraphIndex--;
-      this.renderGraph();
-    } else {
-      this.currentGraphIndex = this.updatedGraphArray.length - 1;
-      this.renderGraph();
-    }
-    this.store.dispatch(new ChooseStatistic(this.currentGraphIndex));
-  }
-
-  switchToNextGraph() {
-    if (!this.updatedGraphArray) return;
-
-    if (this.currentGraphIndex < this.updatedGraphArray.length - 1) {
-      this.currentGraphIndex++;
-      this.renderGraph();
-    } else {
-      this.currentGraphIndex = 0;
-      this.renderGraph();
-    }
-    this.store.dispatch(new ChooseStatistic(this.currentGraphIndex));
-  } */
+  
 
   switchToPreviousGraph() {
     if (!this.chartOptionsArray || !this.chartOptionsArray.length) {
@@ -1107,7 +835,6 @@ export class GraphSelectorComponent implements OnInit {
 
       this.selectedDotIndex = this.currentGraphIndex;
 
-    //this.store.dispatch(new ChooseStatistic(this.currentStatisticIndex));
 
     this.renderGraph();
   }
@@ -1124,7 +851,6 @@ export class GraphSelectorComponent implements OnInit {
     this.selectedDotIndex = this.currentGraphIndex;
 
 
-    //this.store.dispatch(new ChooseStatistic(this.currentStatisticIndex));
 
     this.renderGraph();
   }
@@ -1137,13 +863,6 @@ export class GraphSelectorComponent implements OnInit {
         this.currentGraphIndex
     );
 
-    /* if (!this.updatedGraphArray || !this.chartOptionsArray[this.currentGraphIndex]) {
-      return;
-    } */
-
-    /* if (!this.updatedGraphArray || !this.chartOptionsArray[this.currentGraphIndex]) {
-      return;
-    } */
     
 
     const container = this.chartContainer.nativeElement;
@@ -1173,51 +892,10 @@ export class GraphSelectorComponent implements OnInit {
         this.currentGraphIndex
       ];
 
-      Chart.defaults['color'] = '#a11111';
-
-    // Render the chart with the updated options
-    // Replace 'apexchart' with the appropriate chart component and attributes
-    //this.chart.updateOptions(this.chartOptions);
 
     if (!this.myChart || !this.chartContainer) {
       return;
     }
-
-    /* if (this.chart) {
-      this.chart.destroy();
-    }
- */
-    const element = document.querySelector('body'); // Replace with your actual element selector
-    if (element) {
-      const styles = window.getComputedStyle(element);
-      const backgroundColor = styles.getPropertyValue('--text-color');
-      Chart.defaults.global.defaultFontColor = backgroundColor;
-    }
-
-    /* const ctx = this.myChart.nativeElement.getContext('2d');
-
-    const container = this.chartContainer.nativeElement;
-    const containerWidth = container.offsetWidth;
-    const containerHeight = container.offsetHeight;
-
-    this.myChart.nativeElement.width = containerWidth;
-    this.myChart.nativeElement.height = containerHeight;
-
-    if (!this.updatedGraphArray || this.updatedGraphArray.length === 0) {
-      return;
-    }
-
-    this.myChart.nativeElement.setAttribute('data-cy', this.currentGraphIndex);
-
-    const currentGraph = this.updatedGraphArray[this.currentGraphIndex];
-
-    this.chart = new Chart(ctx, currentGraph);
-    if (this.chart && this.chart.config.type === 'doughnut') {
-      const offset = 30;
-      this.myChart.nativeElement.style.transform = `translateY(${offset}px)`;
-    } else {
-      this.myChart.nativeElement.style.transform = `translateY(0px)`;
-    }*/
   }
 
   getSelectedDots(): number[] {
