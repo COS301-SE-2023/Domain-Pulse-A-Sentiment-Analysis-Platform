cec3276f341cc8bc487d218101d5ad6e40806a7d
chrislom12 <christiaanlombard12@gmail.com>
2023-09-19 20:13:04 +0200

    🚸 (UI) Improved usability

--- frontend/src/app/main/main.component.ts
+++ frontend/src/app/main/main.component.ts
@@ -93,6 +93,7 @@ export class MainComponent implements OnInit {
   }
 
   processpdfUrl(res: string) {
+    if(!res) return;
     const pdfIndex: number = res.lastIndexOf('.pdf');
 
     if (pdfIndex !== -1) {
@@ -132,6 +133,7 @@ export class MainComponent implements OnInit {
       return;
     }
     if (!this.showReportModal) {
+      this.generateReport();
       this.showReportModal = true;
     } else {
       this.showReportModal = false;
