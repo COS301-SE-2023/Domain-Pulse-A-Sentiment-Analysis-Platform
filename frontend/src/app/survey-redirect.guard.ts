96d6a4642ca4af505a266ef45f086cc79c7454fc
Thuthuka Khumalo <thuthuka111@gmail.com>
2023-09-20 12:56:00 +0200

    ♻️ (frontend) test surver redirect gaurd

--- frontend/src/app/survey-redirect.guard.ts
+++ frontend/src/app/survey-redirect.guard.ts
@@ -18,18 +18,26 @@ export class SurveyRedirectGuard {
     | Promise<boolean | UrlTree>
     | boolean
     | UrlTree {
-    const sourceId = route.paramMap.get('source_id');
-    const sourceName = route.paramMap.get('source_name');
+    const sourceId = route.paramMap.get('source_id') || '';
+    const sourceName = route.paramMap.get('source_name') || '';
 
     // Perform the redirection
     const currHost = window.location.host;
-    if (currHost == 'localhost:4200') {
-      // the below should not be hardcoded
-      window.location.href = `http://localhost:8004/ingest/post-review/${sourceId}/${sourceName}`;
-    } else {
-      window.location.href = `${window.location.protocol}://${currHost}/ingest/post-review/${sourceId}/${sourceName}`;
-    }
+    this.redirect(this.decideNewUrl(currHost, sourceId, sourceName));
     // Return false to prevent the route from being activated as the redirection will handle it
     return false;
+  }
+
+  decideNewUrl(currHost: string, sourceId: string, sourceName: string): string {
+    if (currHost == 'localhost:4200') {
+      // the below should not be hardcoded
+      return `http://localhost:8004/ingest/post-review/${sourceId}/${sourceName}`;
+    } else {
+      return `${window.location.protocol}//${currHost}/ingest/post-review/${sourceId}/${sourceName}`;
+    }
+  }
+
+  redirect(str: string): void {
+    window.location.href = str
   }
 }
