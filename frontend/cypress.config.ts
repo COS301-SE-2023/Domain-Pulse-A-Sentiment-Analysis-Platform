fa1ed90c3b3660fe685b13966a37b1e72f79eb8f
Caden Dobson <106334873+cadendobson@users.noreply.github.com>
2023-09-27 23:33:01 +0200

    ✅ (frontend) fixed 2 basic auth e2e test

--- frontend/cypress.config.ts
+++ frontend/cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:4200',
+    baseUrl: 'https://dev.domainpulse.app',
     //'baseUrl': 'http://dev.dp.cos301.thuthuka.me',
     chromeWebSecurity: false,
   },
